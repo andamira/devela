0846e727c8384737723199457db2be72377b8c3c
joseLuís <joseluis@andamira.net>
2023-03-13 21:06:55 +0100

    update subslice fns docs & make inline

--- src/lib.rs
+++ src/lib.rs
@@ -187,34 +187,37 @@ mod std_utils {
     }
 }
 
-/// Returns a subslice at the left of `slice`, with max length `left_len`.
+/// Returns a left subslice of `slice`, with the given maximum `len`.
 ///
 /// If `left_len > slice.len()` it returns the full slice.
-pub fn subslice_left<T>(slice: &[T], left_len: usize) -> &[T] {
+#[inline]
+pub fn subslice_left<T>(slice: &[T], len: usize) -> &[T] {
     let start_idx = 0;
-    let end_idx = (left_len).clamp(start_idx, slice.len());
+    let end_idx = len.clamp(start_idx, slice.len());
     &slice[start_idx..end_idx]
 }
 
-/// Returns a subslice from the right of `slice`, with max length `right_len`.
+/// Returns a right subslice of `slice`, with the given maximum `len`.
 ///
 /// If `left_len > slice.len()` it returns the full slice.
-pub fn subslice_right<T>(slice: &[T], right_len: usize) -> &[T] {
-    let start_idx = slice.len().saturating_sub(right_len);
+#[inline]
+pub fn subslice_right<T>(slice: &[T], len: usize) -> &[T] {
+    let start_idx = slice.len().saturating_sub(len);
     let end_idx = slice.len();
     &slice[start_idx..end_idx]
 }
 
-/// Returns a subslice from the middle of `slice`, with max length `mid_len`.
+/// Returns a middle subslice of `slice`, with the given maximum `len`.
 ///
-/// If `mid_len > slice.len()` it returns the full slice.
+/// If `len > slice.len()` returns the full `slice`.
 ///
 /// In case of a non-perfect middle split, it leaves an extra left character.
-pub fn subslice_mid<T>(slice: &[T], mid_len: usize) -> &[T] {
+#[inline]
+pub fn subslice_middle<T>(slice: &[T], len: usize) -> &[T] {
     let mid_idx = slice.len() / 2;
-    let half_mid_len = mid_len / 2;
-    let start_idx = mid_idx.saturating_sub(half_mid_len);
-    let end_idx = (mid_idx + half_mid_len + (mid_len % 2)).min(slice.len());
+    let half_len = len / 2;
+    let start_idx = mid_idx.saturating_sub(half_len);
+    let end_idx = (mid_idx + half_len + (len % 2)).min(slice.len());
     &slice[start_idx..end_idx]
 }
 
