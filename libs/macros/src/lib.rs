cd5395e539f148e2b98317af2614138acf5ed42e
joseLuís <joseluis@andamira.net>
2023-05-07 11:11:25 +0200

    [macros] rename `include_block` attribute macro to `compile`
    - update docs, add example.

--- libs/macros/src/lib.rs
+++ libs/macros/src/lib.rs
@@ -6,10 +6,24 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-/// Includes the block attached to this attribute macro, only if `argument`
-/// is equal to `true`.
+/// Compiles the associated code only if the provided literal is equal to `true`.
+///
+/// # Examples
+/// ```
+/// extern crate devela_macros;
+/// use devela_macros::compile;
+///
+/// #[compile(true)]
+/// fn compiled() {}
+///
+/// #[compile(no)]
+/// fn not_compiled() {}
+///
+/// compiled()
+/// // not_compiled() // fails to compile if uncommented
+/// ```
 #[proc_macro_attribute]
-pub fn include_block(argument: TokenStream, input: TokenStream) -> TokenStream {
+pub fn compile(argument: TokenStream, input: TokenStream) -> TokenStream {
     if argument.to_string() == "true" {
         input
     } else {
