8ffa250e4adcfb8a582c340e57864ae5ed8eee7e
joseLuís <joseluis@andamira.net>
2023-05-09 17:19:52 +0200

    [macros] improve the `compile` macro
    - allow the negation of the prediate with the `not()` option.
    - update docs and example.

--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,46 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-/// Compiles the associated code only if the provided literal is equal to `true`.
+/// Compiles the associated code only if the inner predicate equals `true`.
+///
+/// Anything other than `true` is considered `false` and the code wont compile.
+///
+/// It supports the `not()` option that makes `true` become `false`
+/// and anything other than `true` become `true`.
 ///
 /// # Examples
 /// ```
-/// extern crate devela_macros;
-/// use devela_macros::compile;
-///
+/// # extern crate devela_macros;
+/// # use devela_macros::compile;
 /// #[compile(true)]
 /// fn compiled() {}
-///
 /// #[compile(no)]
 /// fn not_compiled() {}
 ///
-/// compiled()
-/// // not_compiled() // fails to compile if uncommented
+/// compiled();
+/// // not_compiled() // fails
+///
+/// /* using `not()` */
+/// #[compile(not(false))]
+/// fn compiled2() {}
+/// #[compile(not(true))]
+/// fn not_compiled2() {}
+///
+/// compiled2();
+/// // not_compiled2(); // fails
 /// ```
 #[proc_macro_attribute]
 pub fn compile(argument: TokenStream, input: TokenStream) -> TokenStream {
-    if argument.to_string() == "true" {
+    let condition = match argument.to_string().split_whitespace().next() {
+        Some(arg) if arg == "true" => true,
+        Some(arg) if arg.starts_with("not(") && arg.ends_with(')') => {
+            let inner_arg = &arg[4..arg.len() - 1];
+            inner_arg != "true"
+        }
+        _ => false,
+    };
+
+    if condition {
         input
     } else {
         proc_macro::TokenStream::new()
