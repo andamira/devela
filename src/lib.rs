410c59fb427a3ef5f50c2c7e3fc4f4f69abe52b4
joseLuís <joseluis@andamira.net>
2023-08-20 12:51:02 +0200

    [macros] devela_macros: improve `compile`
    - add `all()` and `any()` argument wrapper modifiers.
    - support nesting.
    - improve docs.

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,12 @@ use proc_macro::TokenStream;
 ///
 /// Anything other than `true` is considered `false` and the code wont compile.
 ///
-/// It supports the following argument wrapper modifiers:
-/// - `not()`: `true` become `false` and anything other than `true` becomes `true`.
-/// - `some()`: `true` become `false` and anything other than `true` becomes `true`.
-/// - `none()`: `true` become `false` and anything other than `true` becomes `true`.
+/// It supports the following argument wrapper modifiers, that can be nested:
+/// - `not()`: returns `true` if the argument is `false`.
+/// - `some()`: returns `true` if there **is** an inner argument.
+/// - `none()`: returns `true` if there is **not** an inner argument.
+/// - `all()`: returns `true` if all the arguments are `true`.
+/// - `any()`: returns `true` if any argument is `true`.
 ///
 /// # Examples
 /// ```
@@ -24,61 +26,113 @@ use proc_macro::TokenStream;
 /// fn not_compiled() {}
 ///
 /// // `not()`:
-///
 /// #[compile(not(other_than_true))]
-/// fn compiled2() {}
+/// fn compiled_not() {}
 /// #[compile(not(true))]
-/// fn not_compiled2() {}
+/// fn not_compiled_not() {}
 ///
-/// // `some()` and `none()`:
-///
+/// // `some()`
 /// #[compile(some(thing))]
-/// fn compiled3() {}
-/// #[compile(none())]
-/// fn compiled4() {}
-///
+/// fn compiled_some() {}
 /// #[compile(some())]
-/// fn not_compiled3() {}
+/// fn not_compiled_some() {}
+///
+/// // `none()`
+/// #[compile(none())]
+/// fn compiled_none() {}
 /// #[compile(none(thing))]
-/// fn not_compiled4() {}
+/// fn not_compiled_none() {}
+///
+/// // `all()`:
+/// #[compile(all(true, true, none(), some(thing), not(none(thing))))]
+/// fn compiled_all() {}
+/// #[compile(all(true, false))]
+/// fn not_compiled_all() {}
+///
+/// // `any()`:
+/// #[compile(any(true, false))]
+/// fn compiled_any() {}
+/// #[compile(any(false, false))]
+/// fn not_compiled_any() {}
+///
+/// // nested:
+/// #[compile(all(true, not(any(some(), none(thing), not(true)))))]
+/// fn compiled_nested() {}
+/// #[compile(all(true, not(any(some(), none(thing), true))))]
+/// fn not_compiled_nested() {}
 ///
 /// compiled();
-/// compiled2();
-/// compiled3();
-/// compiled4();
+/// compiled_not();
+/// compiled_some();
+/// compiled_none();
+/// compiled_all();
+/// compiled_any();
+/// compiled_nested();
+///
 /// // not_compiled()
-/// // not_compiled2();
-/// // not_compiled3();
-/// // not_compiled4();
+/// // not_compiled_not();
+/// // not_compiled_some();
+/// // not_compiled_none();
+/// // not_compiled_all();
+/// // not_compiled_any();
+/// // not_compiled_nested();
 /// ```
 #[proc_macro_attribute]
 pub fn compile(argument: TokenStream, input: TokenStream) -> TokenStream {
-    let condition = match argument.to_string().split_whitespace().next() {
-        Some(arg) if arg == "true" => true,
-
-        // not(arg) returns true only if arg is false
-        Some(arg) if arg.starts_with("not(") && arg.ends_with(')') => {
-            let inner_arg = &arg[4..arg.len() - 1];
-            inner_arg != "true"
-        }
-
-        // some(arg) returns true only if there's an arg
-        Some(arg) if arg.starts_with("some(") && arg.ends_with(')') => {
-            let inner_arg = &arg[5..arg.len() - 1];
-            !inner_arg.is_empty()
-        }
-
-        // none(arg) returns true only if there's not an arg
-        Some(arg) if arg.starts_with("none(") && arg.ends_with(')') => {
-            let inner_arg = &arg[5..arg.len() - 1];
-            inner_arg.is_empty()
-        }
-        _ => false,
-    };
-
-    if condition {
+    if eval_arg(argument.to_string()) {
         input
     } else {
         proc_macro::TokenStream::new()
     }
+}
+
+#[rustfmt::skip]
+fn eval_arg(arg: String) -> bool {
+    if arg == "true" {
+        true
+
+    } else if arg.starts_with("not(") && arg.ends_with(')') {
+        let inner_arg = &arg[4..arg.len() - 1];
+        !eval_arg(inner_arg.to_string())
+
+    } else if arg.starts_with("some(") && arg.ends_with(')') {
+        let inner_arg = &arg[5..arg.len() - 1];
+        !inner_arg.is_empty()
+
+    } else if arg.starts_with("none(") && arg.ends_with(')') {
+        let inner_arg = &arg[5..arg.len() - 1];
+        inner_arg.is_empty()
+
+    } else if arg.starts_with("all(") && arg.ends_with(')') {
+        let inner_args = &arg[4..arg.len() - 1];
+        split_args(inner_args).into_iter().all(eval_arg)
+
+    } else if arg.starts_with("any(") && arg.ends_with(')') {
+        let inner_args = &arg[4..arg.len() - 1];
+        split_args(inner_args).into_iter().any(eval_arg)
+
+    } else {
+        false
+    }
+}
+
+// Argument parser that correctly deals with nested arguments with commas.
+fn split_args(arg: &str) -> Vec<String> {
+    let mut args = Vec::new();
+    let (mut start, mut level) = (0, 0);
+
+    for (i, ch) in arg.chars().enumerate() {
+        match ch {
+            '(' => level += 1,
+            ')' => level -= 1,
+            ',' if level == 0 => {
+                args.push(arg[start..i].trim().to_string());
+                start = i + 1;
+            }
+            _ => {}
+        }
+    }
+
+    args.push(arg[start..].trim().to_string());
+    args
 }
