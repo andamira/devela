f690ca158cf6ec0bc8a9c3a7f4a9f1b1a0a7293d
joseLuís <joseluis@andamira.net>
2023-08-27 10:56:27 +0200

    new `deprecate_feature` macro

--- src/codegen.rs
+++ src/codegen.rs
@@ -79,3 +79,65 @@ macro_rules! paste {
 pub use paste;
 #[doc(hidden)] // dont export this
 pub use paste_crate::paste as _paste;
+
+/// Set a deprecated feature in order to display a warning if it's enabled.
+///
+/// Note that you can only call this macro once per deprecated feature.
+///
+/// # Examples
+/// ```
+/// deprecate_feature![old: "old-feature-1"];
+///
+/// deprecate_feature![old: "old-feature-2", since: "2.0.0"];
+///
+/// deprecate_feature![old: "old-feature-3", new: "new_feature_3"];
+///
+/// deprecate_feature![old: "old-feature-4", new: "new_feature_4", since: "4.0.0"];
+/// ```
+#[macro_export]
+macro_rules! deprecate_feature {
+    (     old:   $old_feature:literal
+      $(, new:   $new_feature:literal )?
+      $(, since:       $since:literal )?
+    ) => {
+        $crate::paste! {
+            // old, !new, !since
+            #[$crate::codegen::compile_attr(
+                all( none($($new_feature)?), none($($since)?) ),
+                deprecated(note = "\nWARNING. `" $old_feature
+                "` feature deprecated." )
+            )]
+
+            // old, !new, since
+            #[$crate::codegen::compile_attr(
+                all( none($($new_feature)?), some($($since)?) ),
+                deprecated( $(since = $since,)? note = "\nWARNING. `" $old_feature
+                "` feature deprecated since version " $($since)? "." )
+            )]
+
+            // old, new, !since
+            #[$crate::codegen::compile_attr(
+                all( some($($new_feature)?), none($($since)?) ),
+                deprecated(note = "\nWARNING. `" $old_feature
+                "` feature deprecated, use `" $($new_feature)? "` instead." )
+            )]
+
+            // old, new, since
+            #[$crate::codegen::compile_attr(
+                all( some($($new_feature)?), some($($since)?) ),
+                deprecated( $(since = $since,)? note = "\nWARNING. `" $old_feature
+                "` feature deprecated since version " $($since)?
+                ", use `" $($new_feature)? "` instead." )
+            )]
+
+            #[cfg(feature = $old_feature)]
+            const fn [<deprecate_feature_ $old_feature:snake>]() {}
+
+            #[cfg(feature = $old_feature)]
+            #[allow(dead_code)]
+            const [<DEPRECATED_ $old_feature:snake:upper>]: ()
+                = [<deprecate_feature_ $old_feature:snake>]();
+        }
+    };
+}
+pub use deprecate_feature;
