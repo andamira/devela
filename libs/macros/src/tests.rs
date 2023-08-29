50173fd44ee29f5f83ad887f559c8716df03b6bf
joseLuís <joseluis@andamira.net>
2023-08-29 11:50:06 +0200

    [macros] improve docs, examples and tests

--- libs/macros/src/tests.rs
+++ libs/macros/src/tests.rs
@@ -6,36 +6,91 @@ use alloc::string::ToString;
 
 #[test]
 fn test_compile_eval() {
+    /* unary */
+
+    // bare
     assert_eq!(compile_eval("true".into()), true);
     assert_eq!(compile_eval("false".into()), false);
     assert_eq!(compile_eval("NOTTRUE".into()), false);
     assert_eq!(compile_eval("".into()), false);
 
-    // eq
+    // not()
+    assert_eq!(compile_eval("not(true)".into()), false);
+    assert_eq!(compile_eval("not(NOTTRUE)".into()), true);
+    assert_eq!(compile_eval("not()".into()), true);
+
+    /* binary */
+
+    // eq()
     assert_eq!(compile_eval("eq(true, true)".into()), true);
     assert_eq!(compile_eval("eq(false, false)".into()), true);
-    assert_eq!(compile_eval("eq(X, X)".into()), true);
-    assert_eq!(compile_eval("eq(AA, BB)".into()), true); // both are false
+    assert_eq!(compile_eval("eq(X, X)".into()), true); // both are `false`
+    assert_eq!(compile_eval("eq(AA, BB)".into()), true); // both are `false`
     assert_eq!(compile_eval("eq(true, false)".into()), false);
 
-    // ne
+    // ne()
     assert_eq!(compile_eval("ne(true, true)".into()), false);
     assert_eq!(compile_eval("ne(false, false)".into()), false);
-    assert_eq!(compile_eval("ne(X, X)".into()), false);
-    assert_eq!(compile_eval("ne(AA, BB)".into()), false); // both are false
+    assert_eq!(compile_eval("ne(X, X)".into()), false); // both are `false`
+    assert_eq!(compile_eval("ne(AA, BB)".into()), false); // both are `false`
     assert_eq!(compile_eval("ne(true, false)".into()), true);
 
-    // same
-    assert_eq!(compile_eval("same(ABC, ABC, ABC)".to_string()), true);
-    assert_eq!(compile_eval("same(ABC, DEF, DEF)".to_string()), false);
-
+    // xor()
     assert_eq!(compile_eval("xor(true, false)".into()), true);
+    assert_eq!(compile_eval("xor(false, true)".into()), true);
     assert_eq!(compile_eval("xor(true, true)".into()), false);
 
-    assert_eq!(compile_eval("xany(true, false)".into()), true);
-    assert_eq!(compile_eval("xany(true, true)".into()), false);
+    /* non-binary */
 
+    // any()
+    assert_eq!(compile_eval("any(true, true, true)".to_string()), true);
+    assert_eq!(compile_eval("any(false, true, true)".to_string()), true);
+    assert_eq!(compile_eval("any(false, false, false)".to_string()), false);
+
+    // all()
+    assert_eq!(
+        compile_eval("all(true, true, true, true)".to_string()),
+        true
+    );
+    assert_eq!(
+        compile_eval("all(true, true, true, false)".to_string()),
+        false
+    );
+
+    // none()
+    assert_eq!(compile_eval("none()".to_string()), true);
+    assert_eq!(compile_eval("none(true)".to_string()), false);
+    assert_eq!(compile_eval("none(false)".to_string()), false);
+    assert_eq!(compile_eval("none(something)".to_string()), false);
+
+    // some()
+    assert_eq!(compile_eval("some(true)".to_string()), true);
+    assert_eq!(compile_eval("some(false)".to_string()), true);
+    assert_eq!(compile_eval("some(something)".to_string()), true);
+    assert_eq!(compile_eval("some()".to_string()), false);
+
+    // diff()
+    assert_eq!(compile_eval("diff(ABC, DEF, ABC)".to_string()), true);
+    assert_eq!(compile_eval("diff(ABC, ABC, ABC)".to_string()), false);
+
+    // same()
+    assert_eq!(compile_eval("same(ABC, ABC, ABC)".to_string()), true);
+    assert_eq!(compile_eval("same(ABC, DEF, ABC)".to_string()), false);
+
+    // xany()
+    assert_eq!(compile_eval("xany(true, true, false)".into()), true);
+    assert_eq!(compile_eval("xany(true, true, true)".into()), false);
+
+    // xodd()
+    assert_eq!(compile_eval("xodd(true)".into()), true);
     assert_eq!(compile_eval("xodd(true, false, false)".into()), true);
     assert_eq!(compile_eval("xodd(true, true, false)".into()), false);
     assert_eq!(compile_eval("xodd(true, true, true)".into()), true);
+    assert_eq!(compile_eval("xodd(true, true, true, true)".into()), false);
+
+    // xodd()
+    assert_eq!(compile_eval("xone(true, false, false, false)".into()), true);
+    assert_eq!(compile_eval("xone(true, true, false, false)".into()), false);
+    assert_eq!(compile_eval("xone(true, true, true, false)".into()), false);
+    assert_eq!(compile_eval("xone(true, true, true, true)".into()), false);
 }
