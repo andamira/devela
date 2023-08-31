2e257446c66f98e48eb55edfd60b33a26d07bd6b
joseLuís <joseluis@andamira.net>
2023-08-31 10:57:11 +0200

    fix how the bytemuck crate is reexported
    - avoid reexporting its hidden `__core` reexport of `core`, since it messes with inner links.
    - optionally support the allocation feature.

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -3,13 +3,17 @@
 //! Memory, extends [`core::mem`].
 //
 
-/// Reexported [`bytemuck`](https://docs.rs/bytemuck) crate.
+/// Reexported [`bytemuck`](https://docs.rs/bytemuck)'s crate types.
 /// Gives small utilities for casting between plain data types.
-///
-#[doc(inline)]
 #[cfg(feature = "bytemuck")]
 #[cfg_attr(feature = "nightly", doc(cfg(feature = "bytemuck")))]
-pub use ::bytemuck;
-#[cfg(feature = "bytemuck")]
-#[doc(no_inline)]
-pub use ::bytemuck::{CheckedBitPattern, NoUninit, PodInOption, ZeroableInOption};
+pub mod bytemuck {
+    pub use ::bytemuck::{
+        checked::{self, CheckedBitPattern},
+        offset_of, AnyBitPattern, Contiguous, NoUninit, Pod, PodCastError, PodInOption,
+        TransparentWrapper, Zeroable, ZeroableInOption,
+    };
+
+    #[cfg(feature = "alloc")]
+    pub use ::bytemuck::allocation::{self, TransparentWrapperAlloc};
+}
