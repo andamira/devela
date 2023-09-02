dc535f4db32747f22411b30b955d9429b93369d8
joseLuís <joseluis@andamira.net>
2023-09-02 22:46:10 +0200

    [macros] minor refactor

--- libs/macros/src/common.rs
+++ libs/macros/src/common.rs
@@ -35,6 +35,8 @@ pub(crate) fn split_args(arg: &str) -> Vec<String> {
 #[rustfmt::skip]
 #[cfg(feature = "alloc")]
 pub(crate) fn compile_eval(arg: String) -> bool {
+    /* unary */
+
     if arg == "true" {
         true
 
@@ -42,21 +44,7 @@ pub(crate) fn compile_eval(arg: String) -> bool {
         let inner_arg = &arg[4..arg.len() - 1];
         !compile_eval(inner_arg.to_string())
 
-    } else if arg.starts_with("some(") && arg.ends_with(')') {
-        let inner_arg = &arg[5..arg.len() - 1];
-        !inner_arg.is_empty()
-
-    } else if arg.starts_with("none(") && arg.ends_with(')') {
-        let inner_arg = &arg[5..arg.len() - 1];
-        inner_arg.is_empty()
-
-    } else if arg.starts_with("all(") && arg.ends_with(')') {
-        let inner_args = &arg[4..arg.len() - 1];
-        split_args(inner_args).into_iter().all(compile_eval)
-
-    } else if arg.starts_with("any(") && arg.ends_with(')') {
-        let inner_args = &arg[4..arg.len() - 1];
-        split_args(inner_args).into_iter().any(compile_eval)
+    /* binary */
 
     } else if arg.starts_with("eq(") && arg.ends_with(')') {
         let inner_args = &arg[3..arg.len() - 1];
@@ -70,21 +58,39 @@ pub(crate) fn compile_eval(arg: String) -> bool {
         args.len() == 2
             && compile_eval(args[0].clone()) != compile_eval(args[1].clone())
 
-    } else if arg.starts_with("same(") && arg.ends_with(')') {
-        let inner_args = &arg[5..arg.len() - 1];
-        let args: Vec<_> = split_args(inner_args);
-        args.iter().all(|b| b == &args[0])
+    } else if arg.starts_with("xor(") && arg.ends_with(')') {
+        let inner_args = &arg[4..arg.len() - 1];
+        let args = split_args(inner_args);
+        args.len() == 2
+            && (compile_eval(args[0].clone()) ^ compile_eval(args[1].clone()))
+
+    /* non-binary */
+
+    } else if arg.starts_with("any(") && arg.ends_with(')') {
+        let inner_args = &arg[4..arg.len() - 1];
+        split_args(inner_args).into_iter().any(compile_eval)
+
+    } else if arg.starts_with("all(") && arg.ends_with(')') {
+        let inner_args = &arg[4..arg.len() - 1];
+        split_args(inner_args).into_iter().all(compile_eval)
+
+    } else if arg.starts_with("none(") && arg.ends_with(')') {
+        let inner_arg = &arg[5..arg.len() - 1];
+        inner_arg.is_empty()
+
+    } else if arg.starts_with("some(") && arg.ends_with(')') {
+        let inner_arg = &arg[5..arg.len() - 1];
+        !inner_arg.is_empty()
 
     } else if arg.starts_with("diff(") && arg.ends_with(')') {
         let inner_args = &arg[5..arg.len() - 1];
         let args: Vec<_> = split_args(inner_args);
         args.iter().any(|b| b != &args[0])
 
-    } else if arg.starts_with("xor(") && arg.ends_with(')') {
-        let inner_args = &arg[4..arg.len() - 1];
-        let args = split_args(inner_args);
-        args.len() == 2
-            && (compile_eval(args[0].clone()) ^ compile_eval(args[1].clone()))
+    } else if arg.starts_with("same(") && arg.ends_with(')') {
+        let inner_args = &arg[5..arg.len() - 1];
+        let args: Vec<_> = split_args(inner_args);
+        args.iter().all(|b| b == &args[0])
 
     // generalization of xor that emphasizes the limited inclusivity property
     } else if arg.starts_with("xany(") && arg.ends_with(')') {
