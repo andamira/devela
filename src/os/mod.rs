6d803cb53bfa22ffb5c6830f9efebe0489fa4ebc
joseLuís <joseluis@andamira.net>
2023-09-12 08:02:27 +0200

    remove cfg check for `os::linux` to be `no_std` compatible

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -5,10 +5,8 @@
 
 pub(crate) mod all {
     #[doc(inline)]
-    #[cfg(any(target_os = "linux", doc))]
     pub use super::linux::all::*;
 }
 
-#[cfg(any(target_os = "linux", doc))]
 #[cfg_attr(feature = "nightly", doc(cfg(target_os = "linux")))]
 pub mod linux;
