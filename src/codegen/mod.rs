1b82fb2e25df326ebeb29c2fcd66c49b64cf3647
joseLuís <joseluis@andamira.net>
2023-09-16 16:35:28 +0200

    refactor `codegen` module

---
+++ src/codegen/mod.rs
@@ -0,0 +1,20 @@
+// devela::codegen
+//
+//! Code generation and meta-programming.
+//
+
+pub(crate) mod all {
+    #[doc(inline)]
+    pub use super::{cif, compile, compile_attr, paste};
+}
+
+pub use ::devela_macros::{cif, compile, compile_attr};
+
+mod deprecate;
+mod paste;
+
+pub use deprecate::deprecate_feature;
+pub use paste::paste;
+
+#[doc(hidden)]
+pub use paste::__paste;
