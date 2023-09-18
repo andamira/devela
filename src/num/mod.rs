a77b00eada369d10ea846a3505091238035754df
joseLuís <joseluis@andamira.net>
2023-09-18 14:19:26 +0200

    reexport `core::num::NonZero*` types from `num` module
    - improve docs.

--- src/num/mod.rs
+++ src/num/mod.rs
@@ -1,6 +1,8 @@
 // devela::num
 //
 //! Numeric types, extends [`core::num`].
+//!
+//! It also reexports the `NonZero*` types from `libcore`.
 //
 
 #[cfg(test)]
@@ -11,3 +13,8 @@ mod non_specific;
 mod range;
 
 pub use {non_range::*, non_specific::*, range::*};
+
+#[doc = "`::core::`[`num`](core::num) signed integer that is known not to equal zero.\n\n---"]
+pub use core::num::{NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize};
+#[doc = "`::core::`[`num`](core::num) unsigned integer that is known not to equal zero.\n\n---"]
+pub use core::num::{NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize};
--- src/sync.rs
+++ src/sync.rs
@@ -1,7 +1,6 @@
 // devela::str
 //
 //! Synchronization, extends [`core::sync`].
-//!
 //
 
 pub use all::*;
@@ -15,16 +14,17 @@ pub mod atomic {
     //!
     //! It also reexports the [`Atomic`] type from the
     //! [`atomic`](https://docs.rs/atomic) crate,
-    //! and some useful definitions from libcore.
+    //! and some useful definitions from `libcore`.
 
+    #[doc = "[`atomic`](https://docs.rs/atomic) crate's generic atomic wrapper type.\n\n---"]
     #[doc(inline)]
     pub use ::atomic::Atomic;
 
     /* Re-export some useful definitions from libcore */
 
-    #[doc = "`libcore`'s atomic fence.\n\n---"]
+    #[doc = "`::core::sync::`[`atomic`](core::sync::atomic) fence.\n\n---"]
     pub use core::sync::atomic::fence as atomic_fence;
 
-    #[doc = "`libcore`'s atomic memory ordering.\n\n---"]
+    #[doc = "`::core::sync::`[`atomic`](core::sync::atomic) memory ordering.\n\n---"]
     pub use core::sync::atomic::Ordering as AtomicOrdering;
 }
