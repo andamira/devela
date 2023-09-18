3aa39aef791d13d20358fb778c8e27b085b1f899
joseLuís <joseluis@andamira.net>
2023-09-18 18:37:44 +0200

    improve `os_*print*` macros to support receiving expressions
    - update comments.

--- src/os/macros.rs
+++ src/os/macros.rs
@@ -5,62 +5,73 @@
 
 use crate::codegen::paste;
 
-// Generates the very similar macros: os_print, os_println, os_eprint, os_eprintln.
+// Generates the macros:
+// - os_print
+// - os_println
+// - os_eprint
+// - os_eprintln
 macro_rules! generate_os_print_macros {
     () => {
         generate_os_print_macros![$ print,
-            "Prints to the standard output" ];
+            "Prints to the standard output.",
+            "nothing"
+        ];
         generate_os_print_macros![$ println,
-            "Prints to the standard output, with a newline," ];
+            "Prints to the standard output, with a newline.",
+            "a newline"
+        ];
         generate_os_print_macros![$ eprint,
-            "Prints to the standard error" ];
+            "Prints to the standard error.",
+            "nothing"
+        ];
         generate_os_print_macros![$ eprintln,
-            "Prints to the standard error, with a newline," ];
+            "Prints to the standard error, with a newline.",
+            "a newline"
+        ];
     };
-    // # Arguments
-    // $d:     the dollar sign passed as a token. This is a trick to be able to nest repetitions.
-    //         WAITING: https://github.com/rust-lang/rust/issues/83527 to be able to use $$
-    //         SOURCE: https://github.com/rust-lang/rust/issues/35853#issuecomment-415993963
+
+    // # arguments:
     //
-    // $name:  the suffix of the generated macro (os_ + $name).
-    //         It is also the name of the called function
-    //         and the name of the similar std macro.
+    // $d:      the dollar sign passed as a token. This is a trick to be able to nest repetitions.
+    //          WAITING: https://github.com/rust-lang/rust/issues/83527 to be able to use $$
+    //          SOURCE: https://github.com/rust-lang/rust/issues/35853#issuecomment-415993963
     //
-    // $doc:   the first line of documentation
+    // $name:   the suffix of the generated macro (os_ + $name).
+    //          It is also the name of the called function
+    //          and the name of the similar std macro.
     //
-    ($d:tt $name:ident, $doc:literal) => { paste! {
+    // $doc:    the first line of documentation
+    // $doc_ln: documentation dependent on the `ln` variant.
+    //
+    ($d:tt $name:ident, $doc:literal, $doc_ln:literal $(,)?) => { paste! {
         #[doc = $doc]
-        #[doc = " using `os::linux::io::`[`" [<linux_ $name>] "`][super::linux::" [<linux_ $name>] "]."]
-        #[doc = ""]
-        #[doc = "Usage is similar to `std::`[`" $name "!`], with the addition of a byte buffer."]
-        #[doc = ""]
+        #[doc = "\n\nLeverages [`" [<linux_ $name>] "`][super::linux::" [<linux_ $name>] "]"]
+        #[doc = ", [`format_buf`][crate::fmt::format_buf]"]
+        #[doc = "and [`str_concat`][crate::str::str_concat]."]
+        ///
+        #[doc = "Usage is similar but not equal to `std::`[`" $name "!`]."]
+        ///
         /// # Examples
         /// ```
         #[doc = "use devela::os::" [<os_ $name>] ";"]
         ///
-        #[doc = [<os_ $name>]"!(); // print a blank line"]
-        #[doc = [<os_ $name>]r#"!("hello world 1!"); // print a literal"#]
-        #[doc = [<os_ $name>]r#"!("hello", "world ", 1_i32, "!"); // print a literal"#]
-        #[doc = [<os_ $name>]r#"!(buf2=16); // create a buffer of 16 bytes"#]
-        #[doc = [<os_ $name>]r#"!(buf2, "hello world {}!", 2); // formatted print using the buffer"#]
-        #[doc = [<os_ $name>]r#"!(buf3=16, "hello world {}!", 3); // create a buffer and print"#]
-        #[doc = [<os_ $name>]r#"!(buf2, "hello world {}!", 4); // reuse any buffer in scope"#]
-        /// for n in 5..8 {
-        #[doc = "    "[<os_ $name>]r#"!(buf3, "hello world {n}!");"#]
-        /// }
+        #[doc = [<os_ $name>]"!(); // prints " $doc_ln " (1st arm)"]
+        #[doc = [<os_ $name>]r#"!("hello world! 2nd arm"); // a literal"#]
+        #[doc = [<os_ $name>]r#"!("hello", " world! ", 3_i32, 'r', "d arm"); // concatenated literals"#]
+        #[doc = [<os_ $name>]r#"!(buf_a=30); // create a buffer with a byte lenght (4th arm)"#]
+        #[doc = [<os_ $name>]r#"!(buf_a, "hello world! {}th arm", 5); // formatted print using the buffer"#]
+        #[doc = [<os_ $name>]r#"!(buf_b=20, "hello world! {}th arm", 6); // create the buffer and print"#]
+        #[doc = [<os_ $name>]r#"!(&format!["{} {}! {}th arm", "hello", "world", 7]);"#
+            " // an expresion returning a &str"]
         /// ```
         ///
-        /// Error Handling
+        /// # Error Handling
         /// If the write fails, it prints an error message and exits with status code 10.
         #[macro_export]
         #[cfg(all(
             any(
-                target_arch = "x86_64",
-                target_arch = "x86",
-                target_arch = "arm",
-                target_arch = "aarch64",
-                target_arch = "riscv32",
-                target_arch = "riscv64"
+                target_arch = "x86_64", target_arch = "x86", target_arch = "arm",
+                target_arch = "aarch64", target_arch = "riscv32", target_arch = "riscv64"
             ),
             feature = "unsafe_os",
             not(miri),
@@ -70,40 +81,45 @@ macro_rules! generate_os_print_macros {
             doc(cfg(all(target_os = "linux", feature = "unsafe_os")))
         )]
         macro_rules! [<os_ $name>] {
-            // print a newline
+            // 1) print a newline
             () => {
                 $crate::os::linux::[<linux_ $name>]("");
             };
 
-            // print a literal
+            // 2) print a literal
             ($str:literal) => {
                 $crate::os::linux::[<linux_ $name>]($str);
             };
 
-            // print concatenated literals
+            // 3) print concatenated literals
             ($d($d str:literal),+ $d(,)?) => {
                 $crate::os::linux::[<linux_ $name>](
                     $crate::str::str_concat!($d($d str,)+)
                 );
             };
 
-            // create a buffer of the given length
+            // 4) create a buffer of the given length
             ($buf:ident = $len:literal) => {
                 let mut $buf = [0u8; $len];
             };
 
-            // create a buffer of the given lenght and print formatted args
-            ($buf:ident = $len:literal, $d($d args:tt)*) => {
-                [<os_ $name>]![$buf = $len];
-
-                [<os_ $name>]![$buf, $d($d args)*];
-            };
-
-            // print formatted args using an existing buffer
+            // 5) print formatted args using an existing buffer
             ($buf:ident, $d($d args:tt)*) => {
                 $crate::os::linux::[<linux_ $name>](
                     $crate::fmt::format_buf![&mut $buf, $d($d args)*].unwrap()
                 );
+            };
+
+            // 6) create a buffer of the given lenght and print formatted args
+            ($buf:ident = $len:literal, $d($d args:tt)*) => {
+                [<os_ $name>]![$buf = $len]; // call 4th arm
+                [<os_ $name>]![$buf, $d($d args)*]; // call 5th arm
+            };
+
+            // 7) print an expression that returns a string slice
+            // (this must be the last arm to avoid conflicts)
+            ($expr:expr) => {
+                $crate::os::linux::[<linux_ $name>]($expr);
             };
         }
 
