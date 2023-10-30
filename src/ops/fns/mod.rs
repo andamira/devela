c8801c84f911ed5139b78ba6629d4d2529eba8a3
joseLuís <joseluis@andamira.net>
2023-10-30 20:46:41 +0100

    add `ops` base related fns
    - add new fns: `count_digits`, `count_digits_sign`, `count_digits_base`, `count_digits_base_sign`, `digital_root`, `digital_root_base`.

--- src/ops/fns/mod.rs
+++ src/ops/fns/mod.rs
@@ -3,8 +3,9 @@
 //! Functions for numeric operations.
 //
 
+mod base;
 mod div_sqrt;
 mod gcd_lcm;
 mod scale_lerp;
 
-pub use {div_sqrt::*, gcd_lcm::*, scale_lerp::*};
+pub use {base::*, div_sqrt::*, gcd_lcm::*, scale_lerp::*};
