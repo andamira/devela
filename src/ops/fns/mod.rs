7cd4dd2f815297aeb3e532af8b644f48124d5c43
joseLuís <joseluis@andamira.net>
2023-10-31 18:52:36 +0100

    new `ops` sort fns
    - new fns: `sort_bubble`, `sort_insertion`, `sort_merge`, `sort_quick_lomuto`, `sort_quick_hoare`, `sort_quick_3way`, `sort_selection`, `sort_shaker`.

--- src/ops/fns/mod.rs
+++ src/ops/fns/mod.rs
@@ -8,5 +8,6 @@ mod div_sqrt;
 mod factors;
 mod gcd_lcm;
 mod scale_lerp;
+mod sort;
 
-pub use {base::*, div_sqrt::*, factors::*, gcd_lcm::*, scale_lerp::*};
+pub use {base::*, div_sqrt::*, factors::*, gcd_lcm::*, scale_lerp::*, sort::*};
