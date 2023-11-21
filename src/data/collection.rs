78584bf89512b79ea81f309aa79a140635b2c221
joseLuís <joseluis@andamira.net>
2023-11-21 11:20:30 +0100

    update Collection trait

--- src/data/collection.rs
+++ src/data/collection.rs
@@ -1,4 +1,4 @@
-// devela::data::trait
+// devela::data::collection
 //
 //!
 //
@@ -8,17 +8,21 @@ pub trait DataCollection {
     /// The element type of the collection.
     type Element;
 
-    /// Returns `true` if the collection is empty, `false` if it's not,
-    /// `None` if that's not well defined for this particular collection.
-    fn collection_is_empty(&self) -> Option<bool>;
-
-    /// Returns `true` if the collection is full, `false` if it's not,
-    /// `None` if that's not well defined for this particular collection.
-    fn collection_is_full(&self) -> Option<bool>;
-
     /// Returns the reserved capacity for elements in the collection.
-    fn collection_capacity(&self) -> usize;
+    ///
+    /// Returns `None` if that's not well defined for this particular collection.
+    fn collection_capacity(&self) -> Option<usize>;
 
     /// Returns the current number of elements in the collection.
     fn collection_len(&self) -> usize;
+
+    /// Returns `true` if the collection is empty, `false` if it's not.
+    ///
+    /// Returns `None` if that's not well defined for this particular collection.
+    fn collection_is_empty(&self) -> Option<bool>;
+
+    /// Returns `true` if the collection is full, `false` if it's not.
+    ///
+    /// Returns `None` if that's not well defined for this particular collection.
+    fn collection_is_full(&self) -> Option<bool>;
 }
