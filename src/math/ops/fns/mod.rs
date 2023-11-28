562c3a4b51b3fc4646949b8148e9887bc2768981
joseLuís <joseluis@andamira.net>
2023-11-28 16:40:51 +0100

    add prime-related functions
    - new fns: `prime_number_theorem`, `prime_is_*`, `prime_nth_*`, `prime_pi_*`, `totient_*`.

--- src/math/ops/fns/mod.rs
+++ src/math/ops/fns/mod.rs
@@ -7,6 +7,7 @@ mod base;
 mod div_sqrt;
 mod factors;
 mod gcd_lcm;
+mod prime;
 mod scale_lerp;
 
-pub use {base::*, div_sqrt::*, factors::*, gcd_lcm::*, scale_lerp::*};
+pub use {base::*, div_sqrt::*, factors::*, gcd_lcm::*, prime::*, scale_lerp::*};
