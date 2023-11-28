64dfe5ef3405d796b2e899b98382680d8cde4f07
joseLuís <joseluis@andamira.net>
2023-11-28 16:49:50 +0100

    impl `std::Error` and `Display` for `MathError`.

--- src/math/error.rs
+++ src/math/error.rs
@@ -21,6 +21,7 @@ pub enum MathError {
     /// Unspecified error.
     ///
     /// When no clearer error can be given.
+    // RETHINK
     Unspecified,
 
     /// An invalid value for the given type.
@@ -30,11 +31,35 @@ pub enum MathError {
     Overflow,
 
     ///
+    // RETHINK
     Underflow,
 }
 
 impl MathError {
     pub(crate) fn notimpl<T>() -> MathResult<T> {
         Err(MathError::NotImplemented)
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(feature = "nightly", doc(cfg(feature = "std")))]
+// WAIT: error_in_core https://github.com/rust-lang/rust/issues/103765
+impl std::error::Error for MathError {}
+
+mod core_impls {
+    use super::MathError;
+    use core::fmt;
+
+    impl fmt::Display for MathError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                MathError::NotImplemented => write!(f, "Not implemented."),
+                MathError::NotSupported => write!(f, "Not supported."),
+                MathError::Unspecified => write!(f, "Unspecified."),
+                MathError::Invalid => write!(f, "Invalid."),
+                MathError::Overflow => write!(f, "Overflow."),
+                MathError::Underflow => write!(f, "Underflow"),
+            }
+        }
     }
 }
