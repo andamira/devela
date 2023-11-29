089b197669ae4c25cb74694ec92f16358be2501d
joseLuís <joseluis@andamira.net>
2023-11-29 10:28:49 +0100

    fix color gamma functions
    - rename `linearize32` to `color_gamma_apply_f32`.
    - rename `nonlinearize32` to `color_gamma_remove_f32`.
    - add the `f64` versions.
    - improve docs, showcase algorithm.

--- src/color/fns.rs
+++ src/color/fns.rs
@@ -1,29 +1,49 @@
 // devela::color::fns
-//
 
-#[cfg(feature = "libm")]
-use crate::num::FloatExt;
-
-/// Applies the `gamma` to an `f32` channel.
-#[inline]
 #[cfg(any(feature = "std", feature = "libm"))]
-#[cfg_attr(feature = "nightly", doc(cfg(any(feature = "std", feature = "libm"))))]
-pub fn linearize32(nonlinear: f32, gamma: f32) -> f32 {
-    if nonlinear >= 0.04045 {
-        return ((nonlinear + 0.055) / (1. + 0.055)).powf(gamma);
-    } else {
-        nonlinear / 12.92
-    }
-}
+use crate::math::FloatExt;
+use crate::meta::{iif, paste};
 
-/// Removes the `gamma` from an `f32` channel.
-#[inline]
-#[cfg(any(feature = "std", feature = "libm"))]
-#[cfg_attr(feature = "nightly", doc(cfg(any(feature = "std", feature = "libm"))))]
-pub fn nonlinearize32(linear: f32, gamma: f32) -> f32 {
-    if linear >= 0.0031308 {
-        return (1.055) * linear.powf(1.0 / gamma) - 0.055;
-    } else {
-        12.92 * linear
-    }
+macro_rules! color_gamma_fns {
+    ($($t:ty),+) => { $( color_gamma_fns![@$t]; )+ };
+    (@$t:ty) => { paste! {
+        #[doc = "Applies the `gamma` *(γ)* to a linear `" $t "` channel to make it non-linear."]
+        ///
+        /// # Algorithm
+        /// $$ \large
+        /// \begin{align}
+        /// \notag f_\text{apply}(c) = \begin{cases}
+        /// 12.92c,
+        ///   & \text{if } c <= 0.0031308 \cr
+        /// 1.055c^{1/\large\gamma} - 0.055,
+        ///   & \text{if } c > 0.0031308 \end{cases} \cr
+        /// \end{align}
+        /// $$
+        #[inline]
+        #[cfg(any(feature = "std", feature = "libm"))]
+        #[cfg_attr(feature = "nightly", doc(cfg(any(feature = "std", feature = "libm"))))]
+        pub fn [<color_gamma_apply_ $t>](c: $t, gamma: $t) -> $t {
+            iif![c <= 0.0031308; 12.92 * c; 1.055 * c.powf(1.0 / gamma) - 0.055]
+        }
+
+        #[doc = "Removes the `gamma` *(γ)* from a non-linear `" $t "` channel to make it linear."]
+        ///
+        /// # Algorithm
+        /// $$ \large
+        /// \begin{align}
+        /// \notag f_\text{remove}(c) = \begin{cases}
+        /// \LARGE\frac{c}{12.92} \large,
+        ///   & \text{if } c <= 0.04045 \cr
+        /// \LARGE\left(\frac{c+0.055}{1.055} \large - 0.055\right)^\gamma,
+        ///   & \text{if } c > 0.04045 \end{cases} \cr
+        /// \end{align}
+        /// $$
+        #[inline]
+        #[cfg(any(feature = "std", feature = "libm"))]
+        #[cfg_attr(feature = "nightly", doc(cfg(any(feature = "std", feature = "libm"))))]
+        pub fn [<color_gamma_remove_ $t>](c: $t, gamma: $t) -> $t {
+            iif![c <= 0.04045; c / 12.92; ((c + 0.055) / (1.055)).powf(gamma)]
+        }
+    }};
 }
+color_gamma_fns!(f32, f64);
