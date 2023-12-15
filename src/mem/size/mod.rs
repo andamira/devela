45886a46a8f8119a6c718e9d3c5d2b912097d614
joseLuís <joseluis@andamira.net>
2023-12-15 10:18:02 +0100

    minor `mem` refactor
    - make `BitSize` always compiled.
    - make `mem_size_of_expr` feature-gated.

--- src/mem/size/mod.rs
+++ src/mem/size/mod.rs
@@ -5,18 +5,18 @@
 
 /* contains always compiled items */
 
-mod expr;
+mod bit;
 mod reexports;
 mod size;
 
 #[allow(unused)]
 #[cfg(not(feature = "mem"))]
-pub use {expr::*, reexports::*, size::*};
+pub use {bit::*, reexports::*, size::*};
 
 /* feature-gated */
 
 #[cfg(feature = "mem")]
-mod bit;
+mod expr;
 
 // re-export private sub-modules
 #[cfg(feature = "mem")]
@@ -24,9 +24,9 @@ pub use {bit::*, expr::*, reexports::*, size::*};
 
 pub(crate) mod all {
     #[doc(inline)]
-    pub use super::{expr::mem_size_of_expr, reexports::*, size::*};
+    pub use super::{bit::*, reexports::*, size::*};
 
     #[doc(inline)]
     #[cfg(feature = "mem")]
-    pub use super::bit::*;
+    pub use super::expr::mem_size_of_expr;
 }
