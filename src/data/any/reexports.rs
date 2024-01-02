c53d424a81a7017a783b21dbd884c8b733a4e80a
joseLuís <joseluis@andamira.net>
2024-01-02 12:30:36 +0100

    fix data::any reexports

--- src/data/any/reexports.rs
+++ src/data/any/reexports.rs
@@ -1,15 +1,15 @@
-// devela::any::reexports
+// devela::data::any::reexports
 //
 //! Reexported items from `core`.
 //
 
 use crate::code::reexport;
 
-reexport! { rust: core::any, local_module: "any",
+reexport! { rust: core::any, local_module: "data",
     doc: "A trait to emulate dynamic typing.",
     Any
 }
-reexport! { rust: core::any, local_module: "any",
+reexport! { rust: core::any, local_module: "data",
     doc: "Represents a globally unique identifier for a type.",
     TypeId
 }
