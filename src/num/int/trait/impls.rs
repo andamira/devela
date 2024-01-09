6bceeb86b912877028b490803b68d9f4e1ecc4c6
joseLuís <joseluis@andamira.net>
2024-01-09 12:59:05 +0100

    refactor NumInt impls macro

--- src/num/int/trait/impls.rs
+++ src/num/int/trait/impls.rs
@@ -19,97 +19,9 @@ macro_rules! impl_int {
     (@i $p:ident) => { paste! {
         // i*
         impl NumInt for $p {
-            /* combinatorics */
-
-            #[inline]
-            fn int_factorial(self) -> Result<Self::Out> {
-                Int(self).factorial().map(|n|n.0) }
-            #[inline]
-            fn int_ref_factorial(&self) -> Result<Self::Out> {
-                Int(*self).factorial().map(|n|n.0) }
-            #[inline]
-            fn int_subfactorial(self) -> Result<Self::Out> {
-                Int(self).subfactorial().map(|n|n.0) }
-            #[inline]
-            fn int_ref_subfactorial(&self) -> Result<Self::Out> {
-                Int(*self).subfactorial().map(|n|n.0) }
-
-            #[inline]
-            fn int_permute(self, r: Self) -> Result<Self::Out> {
-                Int(self).permute(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_permute(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).permute(*r).map(|n|n.0) }
-            #[inline]
-            fn int_permute_rep(self, r: Self) -> Result<Self::Out> {
-                Int(self).permute_rep(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_permute_rep(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).permute_rep(*r).map(|n|n.0) }
-
-            #[inline]
-            fn int_combine(self, r: Self) -> Result<Self::Out> {
-                Int(self).combine(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_combine(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).combine(*r).map(|n|n.0) }
-            #[inline]
-            fn int_combine_rep(self, r: Self) -> Result<Self::Out> {
-                Int(self).combine_rep(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_combine_rep(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).combine_rep(*r).map(|n|n.0) }
-
-            /* digital root */
-
-            #[inline]
-            fn int_digital_root(self) -> Result<Self::Out> {
-                Ok(Int(self).digital_root().0) }
-            #[inline]
-            fn int_ref_digital_root(&self) -> Result<Self::Out> {
-                Ok(Int(*self).digital_root().0) }
-            #[inline]
-            fn int_digital_root_base(self, base: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).digital_root_base(base).0) }
-            #[inline]
-            fn int_ref_digital_root_base(&self, base: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).digital_root_base(*base).0) }
-
-            /* digits */
-
-            #[inline]
-            fn int_digits(self) -> Result<Self::Out> {
-                Ok(Int(self).digits().0) }
-            #[inline]
-            fn int_ref_digits(&self) -> Result<Self::Out> {
-                Ok(Int(*self).digits().0) }
-            #[inline]
-            fn int_digits_sign(self) -> Result<Self::Out> {
-                Ok(Int(self).digits_sign().0) }
-            #[inline]
-            fn int_ref_digits_sign(&self) -> Result<Self::Out> {
-                Ok(Int(*self).digits_sign().0) }
-            #[inline]
-            fn int_digits_base(self, base: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).digits_base(base).0) }
-            #[inline]
-            fn int_ref_digits_base(&self, base: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).digits_base(*base).0) }
-            #[inline]
-            fn int_digits_base_sign(self, base: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).digits_base_sign(base).0) }
-            #[inline]
-            fn int_ref_digits_base_sign(&self, base: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).digits_base_sign(*base).0) }
+            impl_int![common_body_iu];
 
             /* gcd & lcm */
-
-            #[inline]
-            fn int_gcd(self, other: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).gcd(other).0) }
-            #[inline]
-            fn int_ref_gcd(&self, other: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).gcd(*other).0) }
 
             #[inline]
             fn int_gcd_ext(self, other: Self::Rhs) -> Result<[Self::Out; 3]> {
@@ -119,12 +31,6 @@ macro_rules! impl_int {
             fn int_ref_gcd_ext(&self, other: &Self::Rhs) -> Result<[Self::Out; 3]> {
                 let [gcd, b1, b2] = Int(*self).gcd_ext(*other);
                 Ok([gcd.0, b1.0, b2.0]) }
-            #[inline]
-            fn int_lcm(self, other: Self::Rhs) -> Result<Self::Out> {
-                if let Some(res) = Int(self).lcm(other) { Ok(res.0) } else { Err(E::Overflow) } }
-            #[inline]
-            fn int_ref_lcm(&self, other: &Self::Rhs) -> Result<Self::Out> {
-                if let Some(res) = Int(*self).lcm(*other) { Ok(res.0) } else { Err(E::Overflow) } }
         }
     }};
 
@@ -134,110 +40,118 @@ macro_rules! impl_int {
     (@u $p:ident) => { paste! {
         // u*
         impl NumInt for $p {
-            /* combinatorics */
-
-            #[inline]
-            fn int_factorial(self) -> Result<Self::Out> {
-                Int(self).factorial().map(|n|n.0) }
-            #[inline]
-            fn int_ref_factorial(&self) -> Result<Self::Out> {
-                Int(*self).factorial().map(|n|n.0) }
-            #[inline]
-            fn int_subfactorial(self) -> Result<Self::Out> {
-                Int(self).subfactorial().map(|n|n.0) }
-            #[inline]
-            fn int_ref_subfactorial(&self) -> Result<Self::Out> {
-                Int(*self).subfactorial().map(|n|n.0) }
-
-            #[inline]
-            fn int_permute(self, r: Self) -> Result<Self::Out> {
-                Int(self).permute(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_permute(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).permute(*r).map(|n|n.0) }
-            #[inline]
-            fn int_permute_rep(self, r: Self) -> Result<Self::Out> {
-                Int(self).permute_rep(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_permute_rep(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).permute_rep(*r).map(|n|n.0) }
-
-            #[inline]
-            fn int_combine(self, r: Self) -> Result<Self::Out> {
-                Int(self).combine(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_combine(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).combine(*r).map(|n|n.0) }
-            #[inline]
-            fn int_combine_rep(self, r: Self) -> Result<Self::Out> {
-                Int(self).combine_rep(r).map(|n|n.0) }
-            #[inline]
-            fn int_ref_combine_rep(&self, r: &Self) -> Result<Self::Out> {
-                Int(*self).combine_rep(*r).map(|n|n.0) }
-
-            /* digital root */
-
-            #[inline]
-            fn int_digital_root(self) -> Result<Self::Out> {
-                Ok(Int(self).digital_root().0) }
-            #[inline]
-            fn int_ref_digital_root(&self) -> Result<Self::Out> {
-                Ok(Int(*self).digital_root().0) }
-            #[inline]
-            fn int_digital_root_base(self, base: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).digital_root_base(base).0) }
-            #[inline]
-            fn int_ref_digital_root_base(&self, base: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).digital_root_base(*base).0) }
-
-            /* digits */
-
-            #[inline]
-            fn int_digits(self) -> Result<Self::Out> {
-                Ok(Int(self).digits().0) }
-            #[inline]
-            fn int_ref_digits(&self) -> Result<Self::Out> {
-                Ok(Int(*self).digits().0) }
-            #[inline]
-            fn int_digits_sign(self) -> Result<Self::Out> {
-                Ok(Int(self).digits_sign().0) }
-            #[inline]
-            fn int_ref_digits_sign(&self) -> Result<Self::Out> {
-                Ok(Int(*self).digits_sign().0) }
-            #[inline]
-            fn int_digits_base(self, base: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).digits_base(base).0) }
-            #[inline]
-            fn int_ref_digits_base(&self, base: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).digits_base(*base).0) }
-            #[inline]
-            fn int_digits_base_sign(self, base: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).digits_base_sign(base).0) }
-            #[inline]
-            fn int_ref_digits_base_sign(&self, base: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).digits_base_sign(*base).0) }
+            impl_int![common_body_iu];
 
             /* gcd & lcm */
 
             #[inline]
-            fn int_gcd(self, other: Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(self).gcd(other).0) }
+            fn int_gcd_ext(self, other: Self::Rhs) -> Result<[Self::Out; 3]> { E::ns() }
             #[inline]
-            fn int_ref_gcd(&self, other: &Self::Rhs) -> Result<Self::Out> {
-                Ok(Int(*self).gcd(*other).0) }
-
-            #[inline]
-            fn int_gcd_ext(self, _: Self::Rhs) -> Result<[Self::Out; 3]> { E::ns() }
-            #[inline]
-            fn int_ref_gcd_ext(&self, _: &Self::Rhs) -> Result<[Self::Out; 3]> { E::ns() }
-
-            #[inline]
-            fn int_lcm(self, other: Self::Rhs) -> Result<Self::Out> {
-                if let Some(res) = Int(self).lcm(other) { Ok(res.0) } else { Err(E::Overflow) } }
-            #[inline]
-            fn int_ref_lcm(&self, other: &Self::Rhs) -> Result<Self::Out> {
-                if let Some(res) = Int(*self).lcm(*other) { Ok(res.0) } else { Err(E::Overflow) } }
+            fn int_ref_gcd_ext(&self, other: &Self::Rhs) -> Result<[Self::Out; 3]> { E::ns() }
         }
     }};
+
+    // Inner helpers for repeated common bodies for signed and unsigned
+    // ============================================================================================
+    (common_body_iu) => {
+        /* combinatorics */
+
+        #[inline]
+        fn int_factorial(self) -> Result<Self::Out> {
+            Int(self).factorial().map(|n|n.0) }
+        #[inline]
+        fn int_ref_factorial(&self) -> Result<Self::Out> {
+            Int(*self).factorial().map(|n|n.0) }
+        #[inline]
+        fn int_subfactorial(self) -> Result<Self::Out> {
+            Int(self).subfactorial().map(|n|n.0) }
+        #[inline]
+        fn int_ref_subfactorial(&self) -> Result<Self::Out> {
+            Int(*self).subfactorial().map(|n|n.0) }
+
+        #[inline]
+        fn int_permute(self, r: Self) -> Result<Self::Out> {
+            Int(self).permute(r).map(|n|n.0) }
+        #[inline]
+        fn int_ref_permute(&self, r: &Self) -> Result<Self::Out> {
+            Int(*self).permute(*r).map(|n|n.0) }
+        #[inline]
+        fn int_permute_rep(self, r: Self) -> Result<Self::Out> {
+            Int(self).permute_rep(r).map(|n|n.0) }
+        #[inline]
+        fn int_ref_permute_rep(&self, r: &Self) -> Result<Self::Out> {
+            Int(*self).permute_rep(*r).map(|n|n.0) }
+
+        #[inline]
+        fn int_combine(self, r: Self) -> Result<Self::Out> {
+            Int(self).combine(r).map(|n|n.0) }
+        #[inline]
+        fn int_ref_combine(&self, r: &Self) -> Result<Self::Out> {
+            Int(*self).combine(*r).map(|n|n.0) }
+        #[inline]
+        fn int_combine_rep(self, r: Self) -> Result<Self::Out> {
+            Int(self).combine_rep(r).map(|n|n.0) }
+        #[inline]
+        fn int_ref_combine_rep(&self, r: &Self) -> Result<Self::Out> {
+            Int(*self).combine_rep(*r).map(|n|n.0) }
+
+        /* digital root */
+
+        #[inline]
+        fn int_digital_root(self) -> Result<Self::Out> {
+            Ok(Int(self).digital_root().0) }
+        #[inline]
+        fn int_ref_digital_root(&self) -> Result<Self::Out> {
+            Ok(Int(*self).digital_root().0) }
+        #[inline]
+        fn int_digital_root_base(self, base: Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(self).digital_root_base(base).0) }
+        #[inline]
+        fn int_ref_digital_root_base(&self, base: &Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(*self).digital_root_base(*base).0) }
+
+        /* digits */
+
+        #[inline]
+        fn int_digits(self) -> Result<Self::Out> {
+            Ok(Int(self).digits().0) }
+        #[inline]
+        fn int_ref_digits(&self) -> Result<Self::Out> {
+            Ok(Int(*self).digits().0) }
+        #[inline]
+        fn int_digits_sign(self) -> Result<Self::Out> {
+            Ok(Int(self).digits_sign().0) }
+        #[inline]
+        fn int_ref_digits_sign(&self) -> Result<Self::Out> {
+            Ok(Int(*self).digits_sign().0) }
+        #[inline]
+        fn int_digits_base(self, base: Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(self).digits_base(base).0) }
+        #[inline]
+        fn int_ref_digits_base(&self, base: &Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(*self).digits_base(*base).0) }
+        #[inline]
+        fn int_digits_base_sign(self, base: Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(self).digits_base_sign(base).0) }
+        #[inline]
+        fn int_ref_digits_base_sign(&self, base: &Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(*self).digits_base_sign(*base).0) }
+
+        /* gcd & lcm */
+
+        #[inline]
+        fn int_gcd(self, other: Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(self).gcd(other).0) }
+        #[inline]
+        fn int_ref_gcd(&self, other: &Self::Rhs) -> Result<Self::Out> {
+            Ok(Int(*self).gcd(*other).0) }
+
+        #[inline]
+        fn int_lcm(self, other: Self::Rhs) -> Result<Self::Out> {
+            if let Some(res) = Int(self).lcm(other) { Ok(res.0) } else { Err(E::Overflow) } }
+        #[inline]
+        fn int_ref_lcm(&self, other: &Self::Rhs) -> Result<Self::Out> {
+            if let Some(res) = Int(*self).lcm(*other) { Ok(res.0) } else { Err(E::Overflow) } }
+    };
 }
 use impl_int;
