47f2958203121ad39708d5bc14337cbd36606c04
joseLuís <joseluis@andamira.net>
2024-01-21 14:10:46 +0100

    reexport the `Infallible` type as the `NeverErr` type

--- src/error/never.rs
+++ src/error/never.rs
@@ -3,24 +3,16 @@
 //! `never` types.
 //
 
+use crate::code::reexport;
 use core::fmt;
 
-/// The type intended for a [`Result::Err`] variant that can never happen.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum NeverErr {}
+reexport! { rust: core::convert, local_module: "error",
+doc: "The type intended for a [`Result::Err`] variant that can never happen.",
+@Infallible as NeverErr}
 
 /// The type intended for a [`Result::Ok`] variant that can never happen.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum NeverOk {}
-
-#[cfg(feature = "std")]
-impl std::error::Error for NeverErr {}
-
-impl fmt::Display for NeverErr {
-    fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {}
-    }
-}
 
 impl fmt::Display for NeverOk {
     fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
