abfb580f43891584eed822f7c4a4e9d1103d6059
joseLuís <joseluis@andamira.net>
2024-01-27 11:11:15 +0100

    update `sleep4` macro

--- src/work/thread/sleep.rs
+++ src/work/thread/sleep.rs
@@ -1,36 +1,31 @@
 // devela::work::thread::sleep
-//
-//!
-//
 
-/// A more compact `std::thread::`[`sleep`][std::thread::sleep].
+/// A more compact [`thread_sleep`][crate::work::thread::thread_sleep].
 ///
 /// Sleeps for `$ns` seconds + `$ms` milliseconds + `$us` microseconds + `$ns` nanoseconds
 ///
 /// # Examples
 /// ```
 /// # use devela::all::sleep4;
-///
-/// let millis = 1250;
-/// sleep4![millis / 1000, 0, 100 * 5]; // sleeps for 1 second + 500 microseconds
+/// sleep4![1, 0, 500]; // sleeps for 1 second + 500 microseconds
 /// ```
 #[macro_export]
 #[cfg(feature = "std")]
 #[cfg_attr(feature = "nightly", doc(cfg(feature = "std")))]
 macro_rules! sleep4 {
     ($s:expr) => {
-        std::thread::sleep(std::time::Duration::from_secs($s));
+        std::thread::sleep(core::time::Duration::from_secs($s));
     };
     ($s:expr, $ms:expr) => {
-        std::thread::sleep(std::time::Duration::from_millis($s * 1000 + $ms));
+        std::thread::sleep(core::time::Duration::from_millis($s * 1000 + $ms));
     };
     ($s:expr, $ms:expr, $us:expr) => {
-        std::thread::sleep(std::time::Duration::from_micros(
+        std::thread::sleep(core::time::Duration::from_micros(
             $s * 1_000_000 + $ms * 1_000 + $us,
         ));
     };
     ($s:expr, $ms:expr, $us:expr, $ns:expr) => {
-        std::thread::sleep(std::time::Duration::from_nanos(
+        std::thread::sleep(core::time::Duration::from_nanos(
             $s * 1_000_000_000 + $ms * 1_000_000 + $us * 1_000 + $ns,
         ));
     };
