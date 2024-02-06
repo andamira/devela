6746d1854eda888c066e681cbe9cb3abbe227793
joseLuís <joseluis@andamira.net>
2024-02-06 12:37:22 +0100

    update `Stack`
    - make the checked `own_*` methods call the `_unchecked` versions to improve DRY.
    - add methods: `own_nip2`, `own_nip2_unchecked`.
    - fix test.

--- src/data/collections/stack/methods_own.rs
+++ src/data/collections/stack/methods_own.rs
@@ -59,11 +59,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if len == CAP {
             Own::new(self, Err(NotEnoughSpace(Some(1))))
         } else {
-            let mut arr = self.array.into_array_const();
-            arr[len] = e;
-            let mut sta = Self::from_array_const(arr);
-            sta.len = len + 1;
-            Own::new(sta, Ok(()))
+            self.own_push_unchecked(e).into_result_const()
         }
     }
 
@@ -76,7 +72,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
     /// ```
     /// # use devela::all::{DataResult, Stack};
     /// const S: Stack<i32, (), 2> =
-    ///     Stack::new_copied(0).own_push_unchecked(1).own_push_unchecked(2).state;
+    ///     Stack::new_copied(0).own_push_unchecked(1).state.own_push_unchecked(2).state;
     /// assert_eq![S.as_slice(), &[1, 2]];
     /// ```
     #[inline]
@@ -107,11 +103,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if self.len == 0 {
             Own::new(self, Err(NotEnoughElements(Some(1))))
         } else {
-            let arr = self.array.into_array_const();
-            let e = arr[self.len - 1];
-            let mut sta = Self::from_array_const(arr);
-            sta.len = self.len - 1;
-            Own::new(sta, Ok(e))
+            self.own_pop_unchecked().into_result_const()
         }
     }
 
@@ -158,9 +150,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if self.len < 2 {
             Own::new(self, Err(NotEnoughElements(Some(2))))
         } else {
-            let mut arr = self.array.into_array_const();
-            cswap![arr[self.len - 2], arr[self.len - 1]];
-            Own::new(Self::from_array_const(arr), Ok(()))
+            self.own_swap_unchecked().into_result_const()
         }
     }
     /// Swaps the top two stack elements in compile-time, unchecked version.
@@ -201,10 +191,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if self.len < 4 {
             Own::new(self, Err(NotEnoughElements(Some(4))))
         } else {
-            let mut arr = self.array.into_array_const();
-            cswap![arr[self.len - 4], arr[self.len - 2]];
-            cswap![arr[self.len - 3], arr[self.len - 1]];
-            Own::new(Self::from_array_const(arr), Ok(()))
+            self.own_swap2_unchecked().into_result_const()
         }
     }
     /// Swaps the top two pair stack elements in compile-time, unchecked version.
@@ -248,9 +235,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if self.len == 0 {
             Own::new(self, Err(NotEnoughElements(Some(1))))
         } else {
-            let mut sta = self;
-            sta.len -= 1;
-            Own::empty_ok(sta)
+            self.own_drop_unchecked().into_result_const()
         }
     }
     /// Swaps the top two pair stack elements, unchecked version.
@@ -288,9 +273,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if self.len < n {
             Own::new(self, Err(NotEnoughElements(Some(n))))
         } else {
-            let mut sta = self;
-            sta.len -= n;
-            Own::empty_ok(sta)
+            self.own_drop_n_unchecked(n).into_result_const()
         }
     }
     /// Drops the top `n` stack elements, unchecked version.
@@ -334,11 +317,7 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         if self.len < 2 {
             Own::new(self, Err(NotEnoughElements(Some(2))))
         } else {
-            let mut arr = self.array.into_array_const();
-            cswap![arr[self.len - 2], arr[self.len - 1]];
-            let mut sta = Self::from_array_const(arr);
-            sta.len -= 1;
-            Own::empty_ok(sta)
+            self.own_nip_unchecked().into_result_const()
         }
     }
 
@@ -359,6 +338,48 @@ impl<T: Copy, const CAP: usize> Stack<T, (), CAP> {
         cswap![arr[self.len - 2], arr[self.len - 1]];
         let mut sta = Self::from_array_const(arr);
         sta.len -= 1;
+        Own::empty(sta)
+    }
+
+    /// Drops the pair of next stack elements.
+    ///
+    /// `( 1 2 3 4 -- 3 4 )`
+    /// # Errors
+    /// Returns `Own<self,`[`NotEnoughElements`]`>`
+    /// if the stack doesn't contain at least 4 elements.
+    /// # Examples
+    /// ```
+    /// # use devela::all::{Own, Stack};
+    /// const S: Stack<i32, (), 4> = Stack::from_array_const([1, 2, 3, 4]).own_nip2().state_ok();
+    /// assert_eq![S.as_slice(), &[3, 4]];
+    /// ```
+    #[inline]
+    pub const fn own_nip2(self) -> Own<Self, Result<()>> {
+        if self.len < 4 {
+            Own::new(self, Err(NotEnoughElements(Some(4))))
+        } else {
+            self.own_nip2_unchecked().into_result_const()
+        }
+    }
+
+    /// Drops the pair of next stack elements.
+    ///
+    /// `( 1 2 3 4 -- 3 4 )`
+    /// # Panics
+    /// Panics if the stack doesn't contain at least 4 elements.
+    /// # Examples
+    /// ```
+    /// # use devela::all::{Own, Stack};
+    /// const S: Stack<i32, (), 4> = Stack::from_array_const([1, 2, 3, 4]).own_nip2_unchecked().state;
+    /// assert_eq![S.as_slice(), &[3, 4]];
+    /// ```
+    #[inline]
+    pub const fn own_nip2_unchecked(self) -> Own<Self, ()> {
+        let mut arr = self.array.into_array_const();
+        cswap![arr[self.len - 4], arr[self.len - 2]];
+        cswap![arr[self.len - 3], arr[self.len - 1]];
+        let mut sta = Self::from_array_const(arr);
+        sta.len -= 2;
         Own::empty(sta)
     }
 }
