d3cc036a5de8da70cdfd7361517cf61aeb59d33a
joseLuís <joseluis@andamira.net>
2024-02-09 20:21:17 +0100

    add `cargo-script` example

---
+++ examples/cargo-script.rs
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S cargo +nightly -Zscript
+## [dependencies]
+## devela = { version = "0.19", features = ["text"] }
+//
+//! A minimal standalone rust script.
+//!
+//! # Links
+//! - WAIT:[RFC: cargo-script](https://github.com/rust-lang/rfcs/pull/3502)
+//! - WAIT:[Tracking Issue for #3424](https://github.com/rust-lang/cargo/issues/12207)
+//! - WAIT:[RFC: Syntax for embedding cargo-script manifests](https://github.com/rust-lang/rfcs/pull/3503)
+//!     - [frontmatter syntax](https://github.com/rust-lang/rfcs/blob/ce78d/text/3503-frontmatter.md)
+//
+// These are multiple undocumented syntaxes for embedding the manifest ATM:
+// - [Backticks](https://github.com/rust-lang/cargo/pull/13241):
+// ```
+// [dependencies]
+// ```
+// - [dashes](https://github.com/rust-lang/cargo/pull/13241):
+// ---
+// [dependencies]
+// ---
+// - [hashes](https://github.com/rust-lang/cargo/pull/13241):
+// ###
+// [dependencies]
+// ###
+// - [line-prefixes](https://github.com/rust-lang/cargo/pull/13247)
+// ## [dependencies]
+
+use devela::cdbg;
+
+fn main() {
+    cdbg!["hi"];
+}
