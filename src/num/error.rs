fab5eaaa38ddb047f9ceffc93d0d43998c8cd10f
joseLuís <joseluis@andamira.net>
2024-02-18 13:50:38 +0100

    update `Sign`
    - add `None` variant and make it the default.
    - impl `From` between `Sign` and numeric primitives and bool.

--- src/num/error.rs
+++ src/num/error.rs
@@ -75,6 +75,7 @@ mod core_impls {
                         match sign {
                             Sign::Positive => write!(f, "Positive overflow."),
                             Sign::Negative => write!(f, "Negative overflow."),
+                            Sign::None => write!(f, "Unsigned overflow."), // not meaninful
                         }
                     } else {
                         write!(f, "Overflow.")
--- src/num/sign.rs
+++ src/num/sign.rs
@@ -2,19 +2,166 @@
 //
 //! the sign of a number.
 //
+// TOC
+// - definition
+// - impl Into Sign
+// - impl (Try)From Sign
 
-use crate::code::ConstDefault;
+use crate::{
+    code::ConstDefault,
+    num::{Floating, NumError, NumResult as Result},
+};
+use NumError::Invalid;
 
 /// Represents the sign of a number.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
 pub enum Sign {
-    /// A positive sign (+). This is the default value.
+    /// An absence of sign, associated with Zero. (The default)
     #[default]
+    None,
+
+    /// A positive sign (+).
     Positive,
+
     /// A negative sign (-).
     Negative,
 }
 
 impl ConstDefault for Sign {
-    const DEFAULT: Self = Sign::Positive;
+    /// No sign.
+    const DEFAULT: Self = Sign::None;
 }
+
+/* Into Sign */
+
+// helper macro to implement conversion from numbers to Sign
+macro_rules! impl_into_sign {
+    // integer primitives
+    (int: $($int:ty),+) => { $( impl_into_sign![@int: $int]; )+ };
+    (@int: $int:ty) => {
+        impl From<$int> for Sign {
+            /// Returns `None` if 0, `Positive` if > 0 and `Negative` if < 0.
+            #[must_use] #[inline]
+            fn from(n: $int) -> Sign {
+                match n {
+                    0 => Sign::None,
+                    1.. => Sign::Positive,
+                    #[allow(unreachable_patterns)] // for unsigned
+                    _ => Sign::Negative,
+                }
+            }
+        }
+    };
+    // floating-point primitives
+    (float: $($float:ty),+) => { $( impl_into_sign![@float: $float]; )+ };
+    (@float: $float:ty) => {
+        impl From<$float> for Sign {
+            /// Returns `None` if 0.0, `Positive` if > 0 and `Negative` if < 0.
+            #[must_use] #[inline]
+            fn from(n: $float) -> Sign {
+                if Floating::<$float>::is_zero(n) {
+                    Sign::None
+                } else if Floating::<$float>::is_sign_positive(n) {
+                    Sign::Positive
+                } else {
+                    Sign::Negative
+                }
+            }
+        }
+    };
+    // boolean primitive
+    (bool) => {
+        impl From<bool> for Sign {
+            /// Returns `Positive` if `true` and `Negative` if `false`.
+            #[must_use] #[inline]
+            fn from(n: bool) -> Sign {
+                match n {
+                    true => Sign::Positive,
+                    false => Sign::Negative,
+                }
+            }
+        }
+    };
+}
+impl_into_sign![int: u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize];
+impl_into_sign![float: f32, f64];
+impl_into_sign![bool];
+
+/* (Try)From Sign */
+
+// helper macro to implement conversion from Sign to numbers (1, 0, -1)
+macro_rules! impl_from_sign {
+    // signed integer primitives
+    (sint: $($sint:ty),+) => { $( impl_from_sign![@sint: $sint]; )+ };
+    (@sint: $sint:ty) => {
+        impl From<Sign> for $sint {
+            /// Returns 0 if `None`, 1 if `Positive` and -1 if `Negative`.
+            #[must_use] #[inline]
+            fn from(s: Sign) -> $sint {
+                match s {
+                    Sign::None => 0,
+                    Sign::Positive => 1,
+                    Sign::Negative => -1,
+                }
+            }
+        }
+    };
+    // unsigned integer primitives
+    (uint: $($uint:ty),+) => { $( impl_from_sign![@uint: $uint]; )+ };
+    (@uint: $uint:ty) => {
+        impl TryFrom<Sign> for $uint {
+            type Error = NumError;
+
+            /// Returns 0 if `None` and 1 if `Positive`.
+            ///
+            /// # Errors
+            /// Returns [`Invalid`] if the sign is `Negative`.
+            #[inline]
+            fn try_from(s: Sign) -> Result<$uint> {
+                match s {
+                    Sign::None => Ok(0),
+                    Sign::Positive => Ok(1),
+                    Sign::Negative => Err(Invalid),
+                }
+            }
+        }
+    };
+    // floating-point primitives
+    (float: $($float:ty),+) => { $( impl_from_sign![@float: $float]; )+ };
+    (@float: $float:ty) => {
+        impl From<Sign> for $float {
+            /// Returns 0.0 if `None`, 1.0 if `Positive` and -1.0 if `Negative`.
+            #[must_use] #[inline]
+            fn from(s: Sign) -> $float {
+                match s {
+                    Sign::None => 0.0,
+                    Sign::Positive => 1.0,
+                    Sign::Negative => -1.0,
+                }
+            }
+        }
+    };
+    // boolean primitive
+    (bool) => {
+        impl TryFrom<Sign> for bool {
+            type Error = NumError;
+
+            /// Returns `true` if `None` and `false` if `Negative`.
+            ///
+            /// # Errors
+            /// Returns [`Invalid`] if the sign is `None`.
+            #[inline]
+            fn try_from(s: Sign) -> Result<bool> {
+                match s {
+                    Sign::Positive => Ok(true),
+                    Sign::Negative => Ok(false),
+                    Sign::None => Err(Invalid),
+                }
+            }
+        }
+    };
+}
+impl_from_sign![sint: i8, i16, i32, i64, i128, isize];
+impl_from_sign![uint: u8, u16, u32, u64, u128, usize];
+impl_from_sign![float: f32, f64];
+impl_from_sign![bool];
