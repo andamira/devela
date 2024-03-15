1275fd0fe47d2cbbb20ec9661d62d57a18d515d4
joseLuís <joseluis@andamira.net>
2024-03-15 20:18:20 +0100

    update `array_init`
    - add new arms: `init`, `init_heap`.
    - update example.
    - refactor.

--- src/data/collections/array/array_init.rs
+++ src/data/collections/array/array_init.rs
@@ -22,6 +22,12 @@
 /// #[cfg(feature = "unsafe_array")]
 /// assert_eq![[3,6,9], array_init![unsafe_init [i32; 3], |n| (n as i32 + 1) * 3]];
 ///
+/// assert_eq![[2,4,6], array_init![init [i32; 3], "safe", "unsafe_array",
+///     |n| (n as i32 + 1) * 2]];
+/// #[cfg(feature = "alloc")]
+/// assert_eq![Box::new([2,4,6]), array_init![init_heap [i32; 3], "safe", "unsafe_array",
+///     |n| (n as i32 + 1) * 2]];
+///
 /// assert_eq![[7,7,7], array_init![clone [i32; 3], "safe", "unsafe_array", 7]];
 /// assert_eq![[0,0,0], array_init![default [i32; 3], "safe", "unsafe_array"]];
 /// assert_eq![[4,5,6], array_init![iter [i32; 3], "safe", "unsafe_array", vec![4,5,6,7,8]]];
@@ -45,16 +51,16 @@
 #[macro_export]
 macro_rules! array_init {
     (
+    /* safe initializations */
+
     // safe array initialization in the stack
     safe_init [$T:ty; $LEN:expr], $init:expr) => {{
-
         #[allow(clippy::redundant_closure_call)]
         core::array::from_fn(|i| $init(i))
     }};
     (
     // safe array initialization in the heap
     safe_init_heap [$T:ty; $LEN:expr], $init:expr) => {{
-
         let mut v = $crate::_deps::alloc::vec::Vec::<$T>::with_capacity($LEN);
         for i in 0..$LEN {
             #[allow(clippy::redundant_closure_call)]
@@ -64,12 +70,24 @@ macro_rules! array_init {
             panic!("Can't turn the boxed slice into a boxed array")
         })
     }};
+    (
+    // safe array initialization in the stack, compile-time friendly.
+    safe_const_init [$T:ty; $LEN:expr], $const_init:expr, $copiable:expr) => {{
+        let mut arr: [$T; $LEN] = [$copiable; $LEN];
+        let mut i = 0;
+        while i < $LEN {
+            // WAIT: [const_closures](https://github.com/rust-lang/rust/issues/106003)
+            arr[i] = $const_init(i);
+            i += 1;
+        }
+        arr
+    }};
+    (
 
-   (
-    //
+    /* unsafe initializations */
+
     // unsafe array initialization in the stack
     unsafe_init [$T:ty; $LEN:expr], $init:expr) => {{
-
         // SAFETY: array will be fully initialized in the subsequent loop
         let mut arr: [core::mem::MaybeUninit<$T>; $LEN] =
             unsafe { core::mem::MaybeUninit::uninit().assume_init() };
@@ -87,7 +105,6 @@ macro_rules! array_init {
     (
     // unsafe array initialization in the heap
     unsafe_init_heap [$T:ty; $LEN:expr], $init:expr) => {{
-
         let mut v = $crate::_deps::alloc::vec::Vec::<$T>::with_capacity($LEN);
         #[allow(clippy::redundant_closure_call)]
         for i in 0..$LEN { v.push($init(i)); }
@@ -96,25 +113,9 @@ macro_rules! array_init {
         // SAFETY: pointer comes from using `into_raw`, and capacity is right.
         unsafe { Box::from_raw(raw_slice as *mut [$T; $LEN]) }
     }};
-
-    (
-    //
-    // safe array initialization in the stack, compile-time friendly.
-    safe_const_init [$T:ty; $LEN:expr], $const_init:expr, $copiable:expr) => {{
-
-        let mut arr: [$T; $LEN] = [$copiable; $LEN];
-        let mut i = 0;
-        while i < $LEN {
-            // WAIT: [const_closures](https://github.com/rust-lang/rust/issues/106003)
-            arr[i] = $const_init(i);
-            i += 1;
-        }
-        arr
-    }};
     (
     // unsafe array initialization in the stack, compile-time friendly.
     unsafe_const_init [$T:ty; $LEN:expr], $const_init:expr) => {{
-
         // WAIT: [maybe_uninit_uninit_array](https://github.com/rust-lang/rust/issues/96097)
         // SAFETY: array will be fully initialized in the subsequent loop
         let mut arr: [core::mem::MaybeUninit<$T>; $LEN] =
@@ -128,23 +129,38 @@ macro_rules! array_init {
         unsafe { core::mem::transmute_copy::<_, [$T; $LEN]>(&arr) }
     }};
     (
+
+    /* generalized initializations */
+
+    // initialize an array in the stack
+    init [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $init:expr) => {{
+        #[cfg(any(feature = $fsafe, not(feature = $funsafe)))]
+        { array_init![safe_init [$T; $LEN], $init] }
+        #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
+        { array_init![unsafe_init [$T; $LEN], $init] }
+    }};
+    (
+    // initialize an array in the heap
+    init_heap [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $init:expr) => {{
+        #[cfg(any(feature = $fsafe, not(feature = $funsafe)))]
+        { array_init![safe_init_heap [$T; $LEN], $init] }
+        #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
+        { array_init![unsafe_init_heap [$T; $LEN], $init] }
+    }};
+    (
     // initialize an array the stack, compile-time friendly.
     // $copiable is only used by the safe version as temporary placeholder.
-    const_init [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $const_init:expr, $copiable:expr) => {{
-
+    const_init
+    [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $const_init:expr, $copiable:expr) => {{
         #[cfg(any(feature = $fsafe, not(feature = $funsafe)))]
         { array_init![safe_const_init [$T; $LEN], $const_init, $copiable] }
         #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
         { array_init![unsafe_const_init [$T; $LEN], $const_init ] }
     }};
-
-    // ---
-
     (
-    //
+
     // initialize an array in the stack by cloning $clonable
     clone [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $clonable:expr) => {{
-
         #[cfg(any(feature = $fsafe, not(feature = $funsafe)))]
         { array_init![safe_init [$T; $LEN], |_| $clonable.clone()] }
         #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
@@ -153,15 +169,13 @@ macro_rules! array_init {
     (
     // initialize an array in the heap, by cloning $clonable
     clone_heap [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $clonable:expr) => {{
-
         #[cfg(any(feature = $fsafe, not(feature = $funsafe)))]
         { array_init![safe_init_heap [$T; $LEN], |_| $clonable.clone()] }
         #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
         { array_init![unsafe_init_heap [$T; $LEN], |_| $clonable.clone()] }
     }};
-
     (
-    //
+
     // initialize an array in the stack with $T: Default::default()
     default [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal) => {{
 
@@ -173,25 +187,21 @@ macro_rules! array_init {
     (
     // initialize an array in the heap, with $T: Default::default()
     default_heap [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal) => {{
-
         #[cfg(any(feature = $fsafe, not(feature = $funsafe)))]
         { array_init![safe_init_heap [$T; $LEN], |_| <$T>::default()] }
         #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
         { array_init![unsafe_init_heap [$T; $LEN], |_| <$T>::default()] }
     }};
-
     (
     // initialize an array in the stack with $T: ConstDefault::DEFAULT
     const_default [$T:ty; $LEN:expr]) => {{
         [<$T as $crate::code::ConstDefault>::DEFAULT; $LEN]
     }};
-
     (
-    //
+
     // initialize an array in the stack with an IntoIterator<Item = $T> and with
     // $T::default() in case the iterator length is < $LEN, for the remaining elements.
     iter [$T:ty; $LEN:expr], $fsafe:literal, $funsafe:literal, $intoiter:expr) => {{
-
         let mut iterator = $intoiter.into_iter();
         let mut init_closure = |_| {
             if let Some(e) = iterator.next() { e } else { <$T>::default() }
@@ -201,7 +211,6 @@ macro_rules! array_init {
         #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
         { array_init![unsafe_init [$T; $LEN], init_closure] }
     }};
-
     (
     // initialize an array in the heap with an IntoIterator<Item = $T> and with
     // $T::default() in case the iterator length is < $LEN, for the remaining elements.
@@ -216,10 +225,8 @@ macro_rules! array_init {
         #[cfg(all(not(feature = $fsafe), feature = $funsafe))]
         { array_init![unsafe_init_heap [$T; $LEN], init_closure] }
     }};
-
-    // ---
-
     (
+
     // initialize an array by applying $op (in safe mode it first clones $pre)
     // and propagates errors both from $pre and $op.
     preop [$T:ty; $LEN:expr]?, $fsafe:literal, $funsafe:literal, $pre:expr, $op:expr) => {{
