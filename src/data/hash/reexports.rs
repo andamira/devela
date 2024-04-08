c8452965e4e5003cb3cfec1968b60226936af68d
joseLuís <joseluis@andamira.net>
2024-04-08 11:06:50 +0200

    rename hash re-exports
    - rename `BuildHasher` as `HasherBuild`.
    - rename `BuildHasherDefault` as `HasherBuildDefault`.

--- src/data/hash/reexports.rs
+++ src/data/hash/reexports.rs
@@ -7,23 +7,19 @@ use crate::code::reexport;
 
 reexport! { rust: core::hash,
     doc: "A trait for creating instances of [`Hasher`].",
-    BuildHasher
+    @BuildHasher as HasherBuild
 }
 reexport! { rust: core::hash,
-    doc: "Used to create a default [`BuildHasher`] instance for types that implement
+    doc: "Used to create a default [`HasherBuild`] instance for types that implement
         [`Hasher`] and [`Default`]",
-    BuildHasherDefault
+    @BuildHasherDefault as HasherBuildDefault
 }
 reexport! { rust: core::hash,
-    doc: "A trait for hashing an arbitrary stream of bytes..",
+    doc: "A trait for hashing an arbitrary stream of bytes.",
     Hasher
 }
 
-// NOTE: the trait and the derive macro have the same name
-//
-// reexport! { rust: core::hash,
-//     doc: "A hashable type.",
-//     Hash
-// }
+// NOTE: the trait and the derive macro have the same name:
+// reexport! { rust: core::hash, doc: "A hashable type.", Hash }
 /// <span class='stab portability' title='re-exported from rust&#39;s `core`'>`core`</span>
 pub use core::hash::Hash;
