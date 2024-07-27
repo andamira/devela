2f269f693fed4d3df693de1508d15fabf409194c
joseLuís <joseluis@andamira.net>
2024-07-27 19:17:39 +0200

    fix and refactor the `io_copy` function

--- src/sys/io/define_no_std_io/fns.rs
+++ src/sys/io/define_no_std_io/fns.rs
@@ -19,20 +19,31 @@ where
     #[cfg(feature = "unsafe_array")]
     let mut buf = MaybeUninit::<[u8; S]>::uninit();
     let mut written = 0;
+
     loop {
-        let len = match reader.read(&mut buf) {
+        let len = match reader.read({
+            #[cfg(not(feature = "unsafe_array"))]
+            {
+                &mut buf
+            }
+            #[cfg(feature = "unsafe_array")]
+            // SAFETY: The read method will initialize the portion of the buffer it writes to.
+            unsafe {
+                &mut *buf.as_mut_ptr()
+            }
+        }) {
             Ok(0) => return Ok(written),
             Ok(len) => len,
             Err(ref e) if e.kind() == IoErrorKind::Interrupted => continue,
             Err(e) => return Err(e),
         };
+
         #[cfg(not(feature = "unsafe_array"))]
         writer.write_all(&buf[..len])?;
         #[cfg(feature = "unsafe_array")]
         // SAFETY: `buf` has been initialized up to `len` bytes by the `read` operation.
-        unsafe {
-            writer.write_all(&buf.assume_init_ref()[..len])?;
-        }
+        writer.write_all(unsafe { &buf.assume_init_ref()[..len] })?;
+
         written += len as u64;
     }
 }
