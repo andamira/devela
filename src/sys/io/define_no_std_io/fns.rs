fe48b181219ba52fce36d8a08d193799a4fed843
joseLuís <joseluis@andamira.net>
2024-07-27 18:17:07 +0200

    fix: change `unsafe_sys` feature query to `unsafe_array`

--- src/sys/io/define_no_std_io/fns.rs
+++ src/sys/io/define_no_std_io/fns.rs
@@ -8,15 +8,15 @@ use crate::{
 /// Copies the entire contents of a reader into a writer.
 ///
 /// # Features
-/// Makes use of the `unsafe_sys` feature to employ [`MaybeUninit`].
+/// Makes use of the `unsafe_array` feature to employ [`MaybeUninit`].
 pub fn io_copy<R, W, const S: usize>(reader: &mut R, writer: &mut W) -> IoResult<u64>
 where
     R: ?Sized + Read,
     W: ?Sized + Write,
 {
-    #[cfg(not(feature = "unsafe_sys"))]
+    #[cfg(not(feature = "unsafe_array"))]
     let mut buf = [0u8; S];
-    #[cfg(feature = "unsafe_sys")]
+    #[cfg(feature = "unsafe_array")]
     let mut buf = MaybeUninit::<[u8; S]>::uninit();
     let mut written = 0;
     loop {
@@ -26,9 +26,9 @@ where
             Err(ref e) if e.kind() == IoErrorKind::Interrupted => continue,
             Err(e) => return Err(e),
         };
-        #[cfg(not(feature = "unsafe_sys"))]
+        #[cfg(not(feature = "unsafe_array"))]
         writer.write_all(&buf[..len])?;
-        #[cfg(feature = "unsafe_sys")]
+        #[cfg(feature = "unsafe_array")]
         // SAFETY: `buf` has been initialized up to `len` bytes by the `read` operation.
         unsafe {
             writer.write_all(&buf.assume_init_ref()[..len])?;
