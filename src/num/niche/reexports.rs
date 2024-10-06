6a8ad7838be4630630fdb4170c7a186d9e06dd97
joseLuís <joseluis@andamira.net>
2024-10-06 20:02:31 +0200

    re-export `NonZero`, `Saturating` and `Wrapping`

--- src/num/niche/reexports.rs
+++ src/num/niche/reexports.rs
@@ -9,8 +9,19 @@ reexport! { rust: core::num,
     doc: "A signed integer that is known not to equal zero.",
     NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize
 }
-
 reexport! { rust: core::num,
     doc: "An unsigned integer that is known not to equal zero.",
     NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize
+}
+reexport! { rust: core::num,
+    doc: "A value that is known not to equal zero.",
+    NonZero
+}
+reexport! { rust: core::num,
+    doc: "Provides intentionally-saturating arithmetic on `T`.",
+    Saturating
+}
+reexport! { rust: core::num,
+    doc: "Provides intentionally-wrapped arithmetic on T.",
+    Wrapping
 }
