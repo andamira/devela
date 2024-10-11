35d0c9c22f389cb2bf59adfcdada2fc7570c81dd
joseLuís <joseluis@andamira.net>
2024-10-11 13:43:02 +0200

    update `features.sh` script
    - make it work with any crate.
    - fix feature count.

--- tools/features.sh
+++ tools/features.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-# show all features, with nested contents
-# cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "devela") | .features'
+# reliably gets the name of the crate corresponding to the current directory,
+# even within a workspace or from any subdirectory,
+crate_manifest_path=$(cargo locate-project --message-format plain)
+crate_name=$(cargo metadata --format-version 1 --no-deps \
+  | jq -r --arg manifest_path "$crate_manifest_path" \
+  '.packages[] | select(.manifest_path == $manifest_path) | .name')
 
-# show all feature names
-cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "devela") | .features | keys'
+# show the names of all the features
+cargo metadata --format-version=1 --no-deps | jq ".packages[] | select(.name == \"$crate_name\") | .features | keys"
 
-# count all features
-echo -n "total features: "
-cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "devela") | .features | length'
+# show the feature count (for the manifest)
+total=$(cargo metadata --format-version=1 --no-deps | jq ".packages[] | select(.name == \"$crate_name\") | .features | length")
+visible=$(cargo metadata --format-version=1 --no-deps | jq ".packages[] | select(.name == \"$crate_name\") | .features | keys" \
+	| rg '"' | rg -v '"_' | wc -l)
+hidden=$(cargo metadata --format-version=1 --no-deps | jq ".packages[] | select(.name == \"$crate_name\") | .features | keys" \
+	| rg '"_' | wc -l)
 
-echo -n "hidden features: "
-cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "devela") | .features | keys' \
-	| rg '"_' | wc -l
-echo -n "visible features: "
-cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "devela") | .features | keys' \
-	| rg '"' | rg -v '"_' | wc -l
+echo "[features] # total: $total/300, visible: $visible, hidden: $hidden"
