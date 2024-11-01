3b43e40d4c061401ac1581fbcb47c3509aca6ff9
joseLuís <joseluis@andamira.net>
2024-11-01 17:41:42 +0100

    refactor `divisor`
    - update private comments.
    - add missing attribute.

--- src/num/int/divisor.rs
+++ src/num/int/divisor.rs
@@ -13,7 +13,7 @@
 #[allow(unused_imports)]
 use crate::{
     _dep::_core::{fmt, hash, ops},
-    code::{compile, iif, paste},
+    code::{compile, doc_private, iif, paste},
     num::{isize_up, usize_up},
 };
 
@@ -37,20 +37,26 @@ use crate::{
 #[must_use]
 #[derive(Clone, Copy)]
 pub struct Divisor<T> {
-    inner: Inner<T>,
+    inner: DivisorInner<T>,
 }
 
+#[doc = doc_private!()]
+/// Inner representation of [`Divisor`].
 #[derive(Clone, Copy)]
-enum Inner<T> {
+enum DivisorInner<T> {
     Shift(T, u8),
     MultiplyShift(T, T, u8),
     MultiplyAddShift(T, T, u8),
+    /// *Variant only used for signed numbers.*
     #[allow(dead_code)]
-    ShiftAndNegate(T, u8), // only used for signed
+    ShiftAndNegate(T, u8),
+    /// *Variant only used for signed numbers.*
     #[allow(dead_code)]
-    MultiplyAddShiftNegate(T, T, u8), // only used for signed
+    MultiplyAddShiftNegate(T, T, u8),
 }
 
+#[doc = doc_private!()]
+/// Implements [`Divisor`]`<T>` for each enabled integer primitive.
 macro_rules! impl_divisor {
     () => {
         impl_divisor![signed
@@ -64,20 +70,25 @@ macro_rules! impl_divisor {
         ];
     };
 
+    (
+    // (un)signed entry arms
+    //
+    // # Arguments:
     // $t:     the type. E.g. i8.
     // $un:    the unsigned type of the same size. E.g. u8. (only for signed)
     // $up:    the upcasted type. E.g. i16.
     // $unup:  the unsigned upcasted type. E.g. u16. (only for signed)
     // $is_up: upcasted behavior. Y:upcasted | N:not upcasted | PW depends on pointer width == 64
     // $cap:   the capability feature that enables the current implementation.
-    (signed $( $t:ty | $un:ty | $up:ty | $unup:ty : $is_up:ident : $cap:literal),+) => {
+     signed $( $t:ty | $un:ty | $up:ty | $unup:ty : $is_up:ident : $cap:literal),+) => {
         $( impl_divisor![@signed $t|$un|$up|$unup:$is_up:$cap]; )+
     };
     (unsigned $( $t:ty | $up:ty : $is_up:ident : $cap:literal),+) => {
         $( impl_divisor![@unsigned $t|$up:$is_up:$cap]; )+
     };
-
-    (@signed $t:ty | $un:ty | $up:ty | $unup:ty : $is_up:ident : $cap:literal) => {
+    (
+    /* inner arms */
+     @signed $t:ty | $un:ty | $up:ty | $unup:ty : $is_up:ident : $cap:literal) => {
         #[cfg(feature = $cap )]
         impl_divisor![@traits $t];
 
@@ -87,7 +98,9 @@ macro_rules! impl_divisor {
         impl Divisor<$t> {
             impl_divisor![@shared $t|$un|$up|$unup:$is_up:$cap]; // shared methods
 
-            #[must_use]
+            #[doc = doc_private!()]
+            /// Returns the absolute value of the signed primitive as its unsigned equivalent.
+            #[inline] #[must_use]
             const fn abs(n: $t) -> $un {
                 iif![n < 0; ((-1i8) as $un).wrapping_mul(n as $un); n as $un]
             }
@@ -100,7 +113,7 @@ macro_rules! impl_divisor {
             /// # Examples
             /// ```
             /// # use devela::num::Divisor;
-            #[doc = concat!["let d = Divisor::<", stringify![$t], ">::new(-21);"]]
+            #[doc = concat!["let d = Divisor::<", stringify![$t], ">::new(-21).unwrap();"]]
             /// ```
             #[must_use]
             pub const fn new(d: $t) -> Option<Divisor<$t>> {
@@ -110,21 +123,21 @@ macro_rules! impl_divisor {
                     let ud = Self::abs(d);
                     let shift = ud.ilog2() as u8;
                     let inner = if ud.is_power_of_two() {
-                        iif![d > 0; Inner::Shift(d, shift); Inner::ShiftAndNegate(d, shift)]
+                        iif![d > 0; DivisorInner::Shift(d, shift); DivisorInner::ShiftAndNegate(d, shift)]
                     } else {
                         let (mut magic, rem) = Self::div_rem_wide_by_base(1 << (shift - 1), ud);
                         let e = ud - rem;
                         if e < 1 << shift {
-                            Inner::MultiplyShift(d, d.signum() * (magic as $t + 1), shift - 1)
+                            DivisorInner::MultiplyShift(d, d.signum() * (magic as $t + 1), shift - 1)
                         } else {
                             magic *= 2;
                             let (doubled_rem, overflowed) = rem.overflowing_mul(2);
                             iif![doubled_rem >= ud || overflowed; magic += 1];
                             magic += 1;
                             if d > 0 {
-                                Inner::MultiplyAddShift(d, magic as $t, shift)
+                                DivisorInner::MultiplyAddShift(d, magic as $t, shift)
                             } else {
-                                Inner::MultiplyAddShiftNegate(d, -(magic as $t), shift)
+                                DivisorInner::MultiplyAddShiftNegate(d, -(magic as $t), shift)
                             }
                         }
                     };
@@ -144,11 +157,11 @@ macro_rules! impl_divisor {
             #[inline] #[must_use]
             pub const fn get(&self) -> $t {
                 match self.inner {
-                    Inner::Shift(d, _) => d,
-                    Inner::ShiftAndNegate(d, _) => d,
-                    Inner::MultiplyShift(d, _, _) => d,
-                    Inner::MultiplyAddShift(d, _, _) => d,
-                    Inner::MultiplyAddShiftNegate(d, _, _) => d,
+                    DivisorInner::Shift(d, _) => d,
+                    DivisorInner::ShiftAndNegate(d, _) => d,
+                    DivisorInner::MultiplyShift(d, _, _) => d,
+                    DivisorInner::MultiplyAddShift(d, _, _) => d,
+                    DivisorInner::MultiplyAddShiftNegate(d, _, _) => d,
                 }
             }
 
@@ -200,27 +213,27 @@ macro_rules! impl_divisor {
             #[inline] #[must_use]
             pub const fn div_of(&self, n: $t) -> $t {
                 match self.inner {
-                    Inner::Shift(_, shift) => {
+                    DivisorInner::Shift(_, shift) => {
                         let mask = (1 as $t << shift).wrapping_sub(1);
                         let b = (n >> (<$t>::BITS - 1)) & mask;
                         n.wrapping_add(b) >> shift
                     },
-                    Inner::ShiftAndNegate(_, shift) => {
+                    DivisorInner::ShiftAndNegate(_, shift) => {
                         let mask = (1 as $t << shift).wrapping_sub(1);
                         let b = (n >> (<$t>::BITS - 1)) & mask;
                         let t = n.wrapping_add(b) >> shift;
                         t.wrapping_mul(-1)
                     },
-                    Inner::MultiplyShift(_, magic, shift) => {
+                    DivisorInner::MultiplyShift(_, magic, shift) => {
                         let q = Self::mulh(magic, n) >> shift;
                         iif![q < 0; q + 1; q]
                     },
-                    Inner::MultiplyAddShift(_, magic, shift) => {
+                    DivisorInner::MultiplyAddShift(_, magic, shift) => {
                         let q = Self::mulh(magic, n);
                         let t = q.wrapping_add(n) >> shift;
                         iif![t < 0; t + 1; t]
                     },
-                    Inner::MultiplyAddShiftNegate(_, magic, shift) => {
+                    DivisorInner::MultiplyAddShiftNegate(_, magic, shift) => {
                         let q = Self::mulh(magic, n);
                         let t = q.wrapping_add(n.wrapping_mul(-1)) >> shift;
                         iif![t < 0; t + 1; t]
@@ -257,17 +270,17 @@ macro_rules! impl_divisor {
                 } else {
                     let shift = d.ilog2() as u8;
                     let inner = if d.is_power_of_two() {
-                        Inner::Shift(d, shift)
+                        DivisorInner::Shift(d, shift)
                     } else {
                         let (mut magic, rem) = Self::div_rem_wide_by_base(1 << shift, d);
                         let e = d - rem;
                         if e < 1 << shift {
-                            Inner::MultiplyShift(d, magic + 1, shift)
+                            DivisorInner::MultiplyShift(d, magic + 1, shift)
                         } else {
                             magic = magic.wrapping_mul(2);
                             let (doubled_rem, overflowed) = rem.overflowing_mul(2);
                             if doubled_rem >= d || overflowed { magic += 1; }
-                            Inner::MultiplyAddShift(d, magic + 1, shift)
+                            DivisorInner::MultiplyAddShift(d, magic + 1, shift)
                         }
                     };
                     Some(Self { inner })
@@ -285,9 +298,9 @@ macro_rules! impl_divisor {
             #[inline] #[must_use]
             pub const fn get(&self) -> $t {
                 match self.inner {
-                    Inner::Shift(d, _) => d,
-                    Inner::MultiplyShift(d, _, _) => d,
-                    Inner::MultiplyAddShift(d, _, _) => d,
+                    DivisorInner::Shift(d, _) => d,
+                    DivisorInner::MultiplyShift(d, _, _) => d,
+                    DivisorInner::MultiplyAddShift(d, _, _) => d,
                     _ => unreachable![],
                 }
             }
@@ -333,9 +346,9 @@ macro_rules! impl_divisor {
             #[inline] #[must_use]
             pub const fn div_of(&self, n: $t) -> $t {
                 match self.inner {
-                    Inner::Shift(_, shift) => n >> shift,
-                    Inner::MultiplyShift(_, magic, shift) => Self::mulh(magic, n) >> shift,
-                    Inner::MultiplyAddShift(_, magic, shift) => {
+                    DivisorInner::Shift(_, shift) => n >> shift,
+                    DivisorInner::MultiplyShift(_, magic, shift) => Self::mulh(magic, n) >> shift,
+                    DivisorInner::MultiplyAddShift(_, magic, shift) => {
                         let q = Self::mulh(magic, n);
                         let t = ((n - q) >> 1) + q;
                         t >> shift
@@ -353,20 +366,23 @@ macro_rules! impl_divisor {
             pub const fn [<new_ $t>](d: $t) -> Option<Divisor<$t>> { Self::new(d) }
         }
 
-        // helper function to be called from the cold path branch when divisor == 0.
+        #[doc = doc_private!()]
+        /// Helper function to be called from the cold path branch when divisor == 0.
         #[cold] #[inline(never)]
         const fn cold_0_divisor() -> Option<Self> { None }
 
-        // Multiply two words together, returning only the top half of the product.
-        //
-        // Works by extending the factors to 2N-bits, using the built-in 2N-by-2N-bit
-        // multiplication and shifting right to the top half only.
+        #[doc = doc_private!()]
+        /// Multiply two words together, returning only the top half of the product.
+        ///
+        /// Works by extending the factors to 2N-bits, using the built-in 2N-by-2N-bit
+        /// multiplication and shifting right to the top half only.
         #[inline]
         #[compile(any(same($is_up, Y), all(same($is_up, PW), pointer_width_eq(64))))]
         const fn mulh(x: $t, y: $t) -> $t {
             (((x as $up) * (y as $up)) >> <$t>::BITS) as $t
         }
-        // Non-upcasting version, adapted from Figure 8-2 in Hacker's Delight, 2nd Ed.
+        #[doc = doc_private!()]
+        /// Non-upcasting version, adapted from Figure 8-2 in Hacker's Delight, 2nd Ed.
         #[compile(any(same($is_up, N), all(same($is_up, PW), not(pointer_width_eq(64)))))]
         const fn mulh(x: $t, y: $t) -> $t {
             const HALF_WIDTH_BITS: u32 = <$t>::BITS / 2;
@@ -389,12 +405,13 @@ macro_rules! impl_divisor {
             x_high.wrapping_mul(y_high) + w1 + k
         }
 
-        // Divide a 2N-bit dividend by an N-bit divisor with remainder, assuming
-        // that the result fits into N bits and that the lower half of bits of the
-        // dividend are all zero.
-        //
-        // Works by extending the dividend to 2N-bits and then using the built-in
-        // 2N-by-2N-bit division method.
+        #[doc = doc_private!()]
+        /// Divide a 2N-bit dividend by an N-bit divisor with remainder, assuming
+        /// that the result fits into N bits and that the lower half of bits of the
+        /// dividend are all zero.
+        ///
+        /// Works by extending the dividend to 2N-bits and then using the built-in
+        /// 2N-by-2N-bit division method.
         #[compile(any(same($is_up, Y), all(same($is_up, PW), pointer_width_eq(64))))]
         const fn div_rem_wide_by_base(top_half: $un, d: $un) -> ($un, $un) {
             let n = (top_half as $unup) << <$un>::BITS;
@@ -402,7 +419,8 @@ macro_rules! impl_divisor {
             let rem = (n % (d as $unup)) as $un;
             (quot, rem)
         }
-        // Non-upcasting version, adapted from Figure 9-3 in Hacker's Delight, 2nd Ed.
+        #[doc = doc_private!()]
+        /// Non-upcasting version, adapted from Figure 9-3 in Hacker's Delight, 2nd Ed.
         #[compile(any(same($is_up, N), all(same($is_up, PW), not(pointer_width_eq(64)))))]
         const fn div_rem_wide_by_base(top_half: $un, d: $un) -> ($un, $un) {
             const HALF_WORD_BITS: u32 = <$un>::BITS / 2;
