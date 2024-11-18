7272ebd2a1b6a5ef51a3b973d3aec90acc54e39a
joseLuís <joseluis@andamira.net>
2024-11-18 21:13:30 +0100

    remove sys::log for now

--- src/sys/log/mod.rs
+++ src/sys/log/mod.rs
@@ -2,18 +2,3 @@
 //
 //!
 //
-
-mod config; // `LoggerConfig` struct
-mod ext; // `ExtLog` trait
-mod logging; // `Logging` namespace
-mod print; // `LoggerPrint` logger
-mod reexports; // items from the `log` crate
-
-#[allow(unused_imports)]
-pub use {config::*, ext::*, logging::*, print::*, reexports::*};
-
-pub(crate) mod all {
-    #[doc(inline)]
-    #[allow(unused_imports)]
-    pub use super::{config::*, ext::*, logging::*, print::*, reexports::*};
-}
