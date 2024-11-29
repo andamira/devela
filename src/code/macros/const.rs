c115aa471689d88865581d1a28f02149f2139136
joseLuís <joseluis@andamira.net>
2024-11-29 13:22:57 +0100

    improve `CONST` macro
    - add support for multiple definitions.
    - fix to import it from the root.
    - improve examples.

--- src/code/macros/const.rs
+++ src/code/macros/const.rs
@@ -1,38 +1,57 @@
 // devela::code::macros::const
-//
-//!
-//
 
 /// A helper for constructing macro constants.
 ///
 /// # Examples
 /// ```
 /// # use devela::CONST;
+/// // Supports any expresion
 /// CONST!{EXPR = 2 * 15 / 3}
 /// assert_eq![EXPR![], 10u8];
 /// assert_eq![EXPR![], 10i8];
 ///
 /// CONST!{
-///     /// Supports attributes and visibility.
-///     #[macro_export]
-///     pub(crate) ARRAY = [1, 2, 3]
+///     /// Supports docs, attributes, and visibility.
+///     #[macro_export] // attribute needed if public
+///     pub ARRAY = [1, 2, 3]
 /// }
 /// assert_eq![ARRAY![], [1u16, 2, 3]];
 /// assert_eq![ARRAY![], [1i32, 2, 3]];
+///
+/// // Supports multiple definitions, ended by ;
+/// CONST! {
+///     DOC_1 = "Doc 1." ;
+///     pub(crate) DOC_2 = "Doc 2.";
+/// }
+/// assert_eq![DOC_1![], "Doc 1."];
+/// assert_eq![DOC_2![], "Doc 2."];
+///
+/// // A good use-case is for repeated documentation
+/// /// Function 1, version a.
+/// #[doc = DOC_1!()]
+/// pub fn version_1a() {}
+/// /// Function 1, version b.
+/// #[doc = DOC_1!()]
+/// pub fn version_1b() {}
+///
+/// // It also supports…
+/// CONST!{ /* …empty declarations */ }
 /// ```
-#[rustfmt::skip]
+// NOTE: Fixed to use it from the root: https://github.com/rust-lang/rust/pull/52234
+#[doc(hidden)]
 #[macro_export]
-#[cfg_attr(cargo_primary_package, doc(hidden))]
-macro_rules! CONST {
-    (
+macro_rules! _CONST {
+    ($(
         $(#[$attrs:meta])*
         $vis:vis $NAME:ident = $expr:expr
-    ) => {
-        $(#[$attrs])*
-        #[allow(unused_macro)]
-        macro_rules! $NAME { () => { $expr } }
-        $vis use $NAME;
+     );* $(;)?) => {
+        $(
+            $(#[$attrs])*
+            #[allow(unused_macro)]
+            macro_rules! $NAME { () => { $expr } }
+            $vis use $NAME;
+        )*
     };
 }
 #[doc(inline)]
-pub use CONST;
+pub use _CONST as CONST;
