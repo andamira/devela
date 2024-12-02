05de6ba9de78a461650f30ea7cafbe679c2127dc
joseLuís <joseluis@andamira.net>
2024-12-02 12:57:55 +0100

    fix `size_of_expr` tests

--- src/mem/size/expr/test_coro.rs
+++ src/mem/size/expr/test_coro.rs
@@ -3,22 +3,23 @@
 #[cfg(not(miri))]
 #[test]
 fn api_coro() {
-    use super::mem_size_of_expr;
-    use crate::{data::vec, text::String, work::Coroutine};
+    use super::size_of_expr;
+    use crate::{vec_ as vec, Coroutine, String};
 
     pub(super) trait Foo<'a, 'b> {}
     impl<'a> Foo<'a, 'static> for () {}
     impl<'a, 'b> Foo<'a, 'b> for usize {}
 
     let _byte = 0_u8;
-    let _b: usize = mem_size_of_expr!(_byte);
+    let _b: usize = size_of_expr!(_byte);
     assert_eq!(_b, 1);
 
     fn g() -> impl for<'a> Foo<'a, 'static> {}
-    const G: usize = mem_size_of_expr!(g());
+    const G: usize = size_of_expr!(g());
     assert_eq!(G, 0);
 
     fn h() -> impl Coroutine<Return = u32> {
+        #[coroutine]
         || {
             let a = vec![0];
             let _b = String::new();
@@ -26,6 +27,6 @@ fn api_coro() {
             0
         }
     }
-    const H: usize = mem_size_of_expr!(h());
+    const H: usize = size_of_expr!(h());
     assert_eq!(H, 32); // IMPROVE: gives 16 in miri i686
 }
