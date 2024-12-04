513624b012f837feee311cd7275021de31e027e0
joseLuís <joseluis@andamira.net>
2024-12-04 17:57:12 +0100

    refactor `code`
    - define `private` sub-module.
    - separate `macros` reexports.

--- src/code/macros/mod.rs
+++ src/code/macros/mod.rs
@@ -1,17 +1,17 @@
 // devela::code::macros
 //
 //! Utility macros
-//!
-//! # Implementation notes
-//! 1. Several macros are defined hidden, prefixed with `_`, an publicly re-exported
-//!   unprefixed. This fixes able to import them from the root.
-//!   See: <https://github.com/rust-lang/rust/pull/52234#issuecomment-976702997>
-//!   These are: bitfield, capture_last, const, enumset.
-//!
-//! # Documentation for declarative macros
-//! - [The Little Book of Rust Macros](https://veykril.github.io/tlborm/decl-macros.html)
-//! - [Macros By Example](https://doc.rust-lang.org/reference/macros-by-example.html)
-//! - [Specification](https://doc.rust-lang.org/reference/macro-ambiguity.html)
+//
+// # Implementation notes
+// Several macros are defined hidden, prefixed with `_`, an publicly re-exported
+// unprefixed. This fixes able to import them from the root.
+// See: <https://github.com/rust-lang/rust/pull/52234#issuecomment-976702997>
+// These are: bitfield, capture_last, const, enumset.
+//
+// # Documentation for declarative macros
+// - [The Little Book of Rust Macros](https://veykril.github.io/tlborm/decl-macros.html)
+// - [Macros By Example](https://doc.rust-lang.org/reference/macros-by-example.html)
+// - [Specification](https://doc.rust-lang.org/reference/macro-ambiguity.html)
 
 // private
 mod _doc;
@@ -19,9 +19,6 @@ mod _reexport;
 
 #[doc(hidden)]
 pub use paste::__paste;
-
-mod reexports; // re-exported items
-pub use reexports::*;
 
 mod asserts; // assertion macros
 mod capture; // capture_[first|last|tail]!
@@ -35,34 +32,29 @@ mod impl_trait; // impl_trait!
 mod items; // items!, sf!
 mod paste; // paste! wrapped for docs
 mod r#const; // CONST!
-#[allow(unused_imports)]
-pub use {
-    asserts::*, capture::*, cdbg::*, cfg_if::*, cfor::*, deprecate::*, ident::*, iif::*,
-    impl_trait::*, items::*, paste::*, r#const::*,
-};
+mod reexports; // re-exported items
 
 #[cfg(_bit_·)]
-items! {
-    mod enumset;
-    pub use enumset::*; // enumset!
-}
+mod enumset; // enumset!
 
-pub(crate) mod all {
-    #![allow(unused_imports)]
+/* structural access */
 
-    #[doc(inline)]
+#[allow(unused_imports)]
+pub use {doc_inline::*, private::*};
+
+mod doc_inline {
+    #[cfg(_bit_·)]
+    pub use super::enumset::*;
     pub use super::{
         asserts::*, capture::*, cdbg::*, cfg_if::*, cfor::*, deprecate::*, ident::*, iif::*,
         impl_trait::*, items::*, paste::*, r#const::*, reexports::*,
     };
-
-    #[cfg(_bit_·)]
-    #[doc(inline)]
-    pub use super::enumset::*;
 }
-
-#[allow(unused_imports)]
-pub(crate) use private::*;
+pub(crate) mod all {
+    #![allow(unused_imports)]
+    #[doc(inline)]
+    pub use super::doc_inline::*;
+}
 pub(crate) mod private {
     pub(crate) use super::{_doc::*, _reexport::*};
 }
--- src/code/mod.rs
+++ src/code/mod.rs
@@ -9,30 +9,31 @@
 // safety
 #![cfg_attr(feature = "safe_code", forbid(unsafe_code))]
 
-mod reexports; // re-exported items
-pub use reexports::*;
-
 mod any; // dynamic typing and reflection
 mod default; // ConstDefault, Default
 mod r#type; // type_marker!, type_resource, TypeResource, TypeResourced
-pub use {any::all::*, default::*, r#type::*};
+mod reexports; // re-exported items
 
 pub mod macros; // macros: assert*, cdbg, head, items, paste, sf…
 pub mod result;
-#[doc(no_inline)]
+
+/* structural access */
+
 #[allow(unused_imports)]
-pub use {macros::all::*, result::all::*};
+pub use {doc_hidden::*, doc_inline::*, private::*};
 
-pub(super) mod all {
-    #![allow(unused_imports)]
-
-    #[doc(inline)]
-    pub use super::{
-        any::all::*, default::*, macros::all::*, r#type::*, reexports::*, result::all::*,
-    };
+mod doc_inline {
+    pub use super::{any::all::*, default::*, r#type::*, reexports::*};
 }
-
-pub(crate) use private::*;
+mod doc_hidden {
+    #[doc(hidden)]
+    #[doc(no_inline)]
+    pub use super::{macros::all::*, result::all::*};
+}
+pub(super) mod all {
+    #[doc(inline)]
+    pub use super::{doc_hidden::*, doc_inline::*};
+}
 pub(super) mod private {
     pub(crate) use super::macros::private::*;
 }
