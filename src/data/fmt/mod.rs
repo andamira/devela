5f9970d40776d7b4e123a3041d4715dc3ce41b20
joseLuís <joseluis@andamira.net>
2024-12-18 19:09:13 +0100

    update `serde` dependency
    - add `derive` feature.
    - re-export `Serialize` and `Deserialize`.

--- src/data/fmt/mod.rs
+++ src/data/fmt/mod.rs
@@ -3,17 +3,23 @@
 //! Data serialization and deserialization.
 //
 
+mod reexports;
 mod types;
 
-crate::items! { // structural access: _mods, _all
+crate::items! { // structural access: _mods, _all, _always
     #[allow(unused)]
     pub use _mods::*;
+    #[allow(unused)] #[doc(hidden)] #[doc(no_inline)]
+    pub use _always::*;
 
     mod _mods {
-        pub use super::types::*;
+        pub use super::{reexports::*, types::*};
     }
     pub(super) mod _all {
         #[doc(inline)]
+        pub use super::_mods::*;
+    }
+    pub(super) mod _always { #![allow(unused)]
         pub use super::_mods::*;
     }
 }
---
+++ src/data/fmt/reexports.rs
@@ -0,0 +1,11 @@
+// devela::data::fmt::reexports
+//
+//! Reexported items.
+//
+
+use crate::reexport;
+
+reexport! { "dep_serde", "serde", serde,
+    doc: "",
+    Serialize, Deserialize
+}
