13f54c8f165a093e566cedffef35d4183361c3da
joseLuís <joseluis@andamira.net>
2025-03-14 13:11:49 +0100

    improve `ExtFuture::block_on` docs

--- src/work/future/ext.rs
+++ src/work/future/ext.rs
@@ -14,41 +14,49 @@ use {
 
 impl<F: Future> ExtFuture for F {}
 
+#[cfg(feature = "std")]
+crate::CONST! {
+    DOC_BLOCK_ON = r#"Blocks the thread until the future is ready.
+# Example
+```
+# use devela::ExtFuture as _;
+let future = async {};
+let result = future.block_on();
+```
+# Features
+This method is only available if the `dep_portable_atomic_util` feature is **disabled**,
+because its `Arc` type can't be used as a `self` type.
+See [arbitrary_self_types](https://github.com/rust-lang/rust/issues/44874).
+"#;
+}
+
 /// Extension trait providing additional methods for [`Future`]s.
+#[rustfmt::skip]
 pub trait ExtFuture: Future {
-    /// Blocks the thread until the future is ready.
-    ///
-    /// # Example
-    /// ```
-    /// use devela::ExtFuture as _;
-    ///
-    /// let future = async {};
-    /// let result = future.block_on();
-    /// ```
-    /// # Features
-    /// This method is only available if the `dep_portable_atomic_util` feature is **disabled**,
-    /// because its `Arc` type can't be used as a `self` type.
-    /// See [arbitrary_self_types](https://github.com/rust-lang/rust/issues/44874).
-    ///
+    #[doc = DOC_BLOCK_ON!()]
     #[doc = crate::doc_!(vendor: "pollster")]
-    #[rustfmt::skip]
     #[cfg(feature = "std")]
     #[cfg_attr(nightly_doc, doc(cfg(feature = "std")))]
     #[cfg(not(feature = "dep_portable_atomic_util"))]
     #[cfg_attr(nightly_doc, doc(cfg(not(feature = "dep_portable_atomic_util"))))]
     // WAIT: [arbitrary_self_types](https://github.com/rust-lang/rust/pull/135881)
     fn block_on(self) -> Self::Output where Self: Sized { future_block(self) }
+    #[doc = DOC_BLOCK_ON!()]
+    #[doc = crate::doc_!(vendor: "pollster")]
+    #[cfg(all(feature = "std", feature = "dep_portable_atomic_util"))]
+    #[cfg_attr(nightly_doc, doc(cfg(feature = "std")))]
+    #[cfg_attr(nightly_doc, doc(cfg(not(feature = "dep_portable_atomic_util"))))]
+    fn block_on(self) -> Self::Output where Self: Sized {
+        panic!("block_on is unavailable when `dep_portable_atomic_util` is enabled.");
+    }
 
     /// Creates a future which never resolves.
-    #[rustfmt::skip]
     fn pending<T>() -> FuturePending<T> { pending() }
 
     /// Creates a future that wraps a `function` returning [`TaskPoll`].
-    #[rustfmt::skip]
     fn poll_fn<T, F>(function: F) -> FuturePollFn<F>
     where F: FnMut(&mut TaskContext<'_>) -> TaskPoll<T> { poll_fn(function) }
 
     /// Creates a future that is immediately ready with a `value`.
-    #[rustfmt::skip]
     fn ready<T>(value: T) -> FutureReady<T> { ready(value) }
 }
