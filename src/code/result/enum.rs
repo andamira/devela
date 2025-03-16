186ed2d75e13f5c1cfa6ff4ca8f6b4db94477bb2
joseLuís <joseluis@andamira.net>
2025-03-16 15:04:31 +0100

    update `Enum`
    - automate the generation of the `map_` methods.
    - misc. refactors and fixes.
    - update docs.

--- src/code/result/enum.rs
+++ src/code/result/enum.rs
@@ -1,28 +1,36 @@
 // devela::code::result::enum
 //
-//! Define the [`Enum`] type.
+//! Defines the [`Enum`] type.
 //
+// TOC
+// - enum Enum
+// - impl methods
+// - macro impl_enum! helper
+// - tests
 
 use crate::{iif, ExtAny};
 
-/// A statically sized selection enum allowing up to four possible types.
+/// A generic, parameterized *enum* for expressing structured alternatives.
 ///
 /// Variants are expected to be **contiguous**, meaning `()` (unit types)
-/// should only appear at the **end**. Use [`Self::validate`] to check for correctness.
+/// should only appear at the **end**.
+///
+/// The **first variant** (`A`) is considered the default,
+/// implementing [`Default`] when `A: Default`.
 #[non_exhaustive]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum Enum<A, B, C = (), D = (), E = (), F = ()> {
-    /// The first selection. (The default)
+    /// The first variant (default).
     A(A),
-    /// The second selection.
+    /// The second variant.
     B(B),
-    /// The third selection.
+    /// The third variant.
     C(C),
-    /// The fourth selection.
+    /// The fourth variant.
     D(D),
-    /// The fifth selection.
+    /// The fifth variant.
     E(E),
-    /// The sixth selection.
+    /// The sixth variant.
     F(F),
 }
 impl<A: Default, B, C, D, E, F> Default for Enum<A, B, C, D, E, F> {
@@ -32,6 +40,19 @@ impl<A: Default, B, C, D, E, F> Default for Enum<A, B, C, D, E, F> {
     }
 }
 
+/// General methods.
+impl<A: 'static, B: 'static, C: 'static, D: 'static, E: 'static, F: 'static>
+    Enum<A, B, C, D, E, F>
+{
+    impl_enum!(methods_general: A, B, C, D, E, F);
+}
+
+/// # Field-specific methods.
+impl<A, B, C, D, E, F> Enum<A, B, C, D, E, F> {
+    impl_enum!(methods_field_access: A, B, C, D, E, F);
+    impl_enum!(methods_map: A, B, C, D, E, F);
+}
+
 /// helper for implementing methods for Enum
 ///
 /// Args:
@@ -39,40 +60,13 @@ impl<A: Default, B, C, D, E, F> Default for Enum<A, B, C, D, E, F> {
 macro_rules! impl_enum {
     (
     // implements:
-    // - is_a
-    // - as_ref_a
-    // - as_mut_a
-    methods1: $($T:ident),+) => {
-        $( impl_enum! { @methods1: $T } )+
-    };
-    (@methods1: $T:ident) => { $crate::paste! {
-        #[doc = "Returns `true` if the value is of type [`Self::" $T "`]"]
-        pub fn [<is_ $T:lower>](&self) -> bool { matches!(self, Enum::$T(_)) }
-
-        #[doc = "Returns the inner `" $T "` value, if present."]
-        pub fn [<into_ $T:lower>](self) -> Option<$T> {
-            iif![let Self::$T([<$T:lower>]) = self; Some([<$T:lower>]); None]
-        }
-        #[doc = "Returns a reference to the inner `" $T "` value, if present."]
-        pub fn [<as_ref $T:lower>](&self) -> Option<&$T> {
-            iif![let Self::$T([<$T:lower>]) = self; Some([<$T:lower>]); None]
-        }
-        #[doc = "Returns a reference to the inner `" $T "` value, if present."]
-        pub fn [<as_mut $T:lower>](&mut self) -> Option<&mut $T> {
-            iif![let Self::$T([<$T:lower>]) = self; Some([<$T:lower>]); None]
-        }
-    }};
-    // implements:
+    // - len
+    // - is_empty
     // - validate
     // - first_non_unit
     // - variants_count
     // - eq_variant
-    (methods2: $($T:ident),+) => {
-
-        // pub fn variant_name() -> &'static str {
-        //     $( if <$T>::type_id() != <()>::type_id() { count += 1} )+
-        // }
-
+    methods_general: $($T:ident),+) => {
         /// Returns the number of active (non-`()` type) variants.
         // WAIT: [const_type_id](https://github.com/rust-lang/rust/issues/77125)
         // WAIT: [variant_count](https://github.com/rust-lang/rust/issues/73662)
@@ -99,64 +93,84 @@ macro_rules! impl_enum {
             true
         }
     };
-}
+    (
+    // implements:
+    // - is_*
+    // - into_*
+    // - as_ref_*
+    // - as_mut_*
+    methods_field_access: $($T:ident),+) => {
+        $( impl_enum! { @methods_field_access: $T } )+
+    };
+    (@methods_field_access: $T:ident) => { $crate::paste! {
+        #[doc = "Returns `true` if the value is of type [`" $T "`][Self::" $T "]"]
+        pub fn [<is_ $T:lower>](&self) -> bool { matches!(self, Enum::$T(_)) }
 
-impl<A, B, C, D, E, F> Enum<A, B, C, D, E, F> {
-    impl_enum!(methods1: A, B, C, D, E, F);
-}
-impl<A: 'static, B: 'static, C: 'static, D: 'static, E: 'static, F: 'static>
-    Enum<A, B, C, D, E, F>
-{
-    impl_enum!(methods2: A, B, C, D, E, F);
-}
+        #[doc = "Returns the inner `" $T "` value, if present."]
+        pub fn [<into_ $T:lower>](self) -> Option<$T> {
+            iif![let Self::$T([<$T:lower>]) = self; Some([<$T:lower>]); None]
+        }
+        #[doc = "Returns a reference to the inner `" $T "` value, if present."]
+        pub fn [<as_ref_ $T:lower>](&self) -> Option<&$T> {
+            iif![let Self::$T([<$T:lower>]) = self; Some([<$T:lower>]); None]
+        }
+        #[doc = "Returns a reference to the inner `" $T "` value, if present."]
+        pub fn [<as_mut_ $T:lower>](&mut self) -> Option<&mut $T> {
+            iif![let Self::$T([<$T:lower>]) = self; Some([<$T:lower>]); None]
+        }
+    }};
+    (
+    // implements:
+    // - map_*
+    methods_map: $first:ident $(, $rest:ident)*) => {
+        // For the first variant, the `$before` list is empty.
+        impl_enum!(@methods_map: $first, (), ($($rest),*));
+        // Then, delegate to the helper macro with the first element as the accumulator.
+        impl_enum!(@methods_map_helper: ($first), ($($rest),*));
 
-// methods difficult to implement automatically
-impl<A, B, C, D, E, F> Enum<A, B, C, D, E, F> {
-    /// Transforms the inner `A` value using `f`, leaving other variants unchanged.
-    pub fn map_a<NEW>(self, f: impl FnOnce(A) -> NEW) -> Enum<NEW, B, C, D, E, F> {
-        match self {
-            Self::A(a) => Enum::A(f(a)),
-            Self::B(b) => Enum::B(b),
-            Self::C(c) => Enum::C(c),
-            Self::D(d) => Enum::D(d),
-            Self::E(e) => Enum::E(e),
-            Self::F(f) => Enum::F(f),
+        // NOTE: generates something like the following (e.g. for 6 variants):
+        //
+        // impl_map_method!(A, (/*$before*/), (B, C, D, E, F));
+        // impl_map_method!(B, (A), (C, D, E, F));
+        // impl_map_method!(C, (A, B), (D, E, F));
+        // impl_map_method!(D, (A, B, C), (E, F));
+        // impl_map_method!(E, (A, B, C, D), (F));
+        // impl_map_method!(F, (A, B, C, D, E), (/*$after*/));
+    };
+    (
+    @methods_map: $T:ident, ( $($before:ident),* ), ( $($after:ident),* )) => { $crate::paste! {
+        #[doc = "Transforms the inner `" $T "` value using `f`, leaving other variants unchanged."]
+        pub fn [<map_ $T:lower>]<NEW>(self, f: impl FnOnce($T) -> NEW)
+            -> Enum< $($before,)* NEW, $($after,)* > {
+            match self {
+                $( Self::$before(val) => Enum::$before(val), )*
+                Self::$T(val) => Enum::$T(f(val)),
+                $( Self::$after(val) => Enum::$after(val), )*
+            }
         }
-    }
-    /// Transforms the inner `B` value using `f`, leaving other variants unchanged.
-    pub fn map_b<NEW>(self, f: impl FnOnce(B) -> NEW) -> Enum<A, NEW, C, D, E, F> {
-        match self {
-            Self::A(a) => Enum::A(a),
-            Self::B(b) => Enum::B(f(b)),
-            Self::C(c) => Enum::C(c),
-            Self::D(d) => Enum::D(d),
-            Self::E(e) => Enum::E(e),
-            Self::F(f) => Enum::F(f),
-        }
-    }
-    /// Transforms the inner `C` value using `f`, leaving other variants unchanged.
-    pub fn map_c<NEW>(self, f: impl FnOnce(C) -> NEW) -> Enum<A, B, NEW, D, E, F> {
-        match self {
-            Self::A(a) => Enum::A(a),
-            Self::B(b) => Enum::B(b),
-            Self::C(c) => Enum::C(f(c)),
-            Self::D(d) => Enum::D(d),
-            Self::E(e) => Enum::E(e),
-            Self::F(f) => Enum::F(f),
-        }
-    }
-    /// Transforms the inner `D` value using `f`, leaving other variants unchanged.
-    pub fn map_d<NEW>(self, f: impl FnOnce(D) -> NEW) -> Enum<A, B, C, NEW, E, F> {
-        match self {
-            Self::A(a) => Enum::A(a),
-            Self::B(b) => Enum::B(b),
-            Self::C(c) => Enum::C(c),
-            Self::D(d) => Enum::D(f(d)),
-            Self::E(e) => Enum::E(e),
-            Self::F(f) => Enum::F(f),
-        }
-    }
+        // NOTE: Generates methods like the following (e.g. for variant C, of 6):
+        //
+        // pub fn map_c<NEW>(self, f: impl FnOnce(C) -> NEW) -> Enum<A, B, NEW, D, E, F> {
+        //     match self {
+        //         Self::A(a) => Enum::A(a),    // $before
+        //         Self::B(b) => Enum::B(b),    // …
+        //         Self::C(c) => Enum::C(f(c)), // $T
+        //         Self::D(d) => Enum::D(d),    // $after
+        //         Self::E(e) => Enum::E(e),    // …
+        //         Self::F(f) => Enum::F(f),    // …
+        //     }
+        // }
+    }};
+    // Stop when there are no types left in the `$after` list.
+    (@methods_map_helper: ($($before:ident),*), ()) => {};
+    // Recursively take the next type as the current one.
+    (@methods_map_helper: ($($before:ident),*), ($first:ident $(, $rest:ident)*)) => {
+        impl_enum!(@methods_map: $first, ($($before),*), ($($rest),*));
+        // Append the current type to the "before" list and continue.
+        impl_enum!(@methods_map_helper: ($($before,)* $first), ($($rest),*));
+    };
 }
+use impl_enum;
 
 #[cfg(test)]
 mod tests {
@@ -169,6 +183,8 @@ mod tests {
     fn len() {
         assert_eq![Bytes::len(), 2];
         assert_eq![Unums::len(), 4];
+        assert![!Bytes::is_empty()];
+        assert![Enum::<(), ()>::is_empty()];
     }
     #[test]
     fn validate() {
