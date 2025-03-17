44e2301b9f600ead011f20288d687f294e239380
joseLuís <joseluis@andamira.net>
2025-03-17 12:54:01 +0100

    refactor `impl_enum!`
    - automate `Enum` defition, `Default` impl and all impl blocks.
    - add more tests.
    - update docs.

--- src/code/result/enum.rs
+++ src/code/result/enum.rs
@@ -2,64 +2,47 @@
 //
 //! Defines the [`Enum`] type.
 //
-// TOC
-// - enum Enum
-// - impl methods
-// - macro impl_enum! helper
-// - tests
 
 use crate::{iif, ExtAny};
 
-/// A generic, parameterized *enum* for expressing structured alternatives.
-///
-/// Variants are expected to be **contiguous**, meaning `()` (unit types)
-/// should only appear at the **end**.
-///
-/// The **first variant** (`A`) is considered the default,
-/// implementing [`Default`] when `A: Default`.
-#[non_exhaustive]
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum Enum<A, B, C = (), D = (), E = (), F = ()> {
-    /// The first variant (default).
-    A(A),
-    /// The second variant.
-    B(B),
-    /// The third variant.
-    C(C),
-    /// The fourth variant.
-    D(D),
-    /// The fifth variant.
-    E(E),
-    /// The sixth variant.
-    F(F),
-}
-impl<A: Default, B, C, D, E, F> Default for Enum<A, B, C, D, E, F> {
-    /// Defaults to [`Self::A`] using `A::default()`.
-    fn default() -> Self {
-        Enum::A(A::default())
-    }
-}
+impl_enum!(A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10, K:11, L:12); // 12 variants
 
-/// General methods.
-impl<A: 'static, B: 'static, C: 'static, D: 'static, E: 'static, F: 'static>
-    Enum<A, B, C, D, E, F>
-{
-    impl_enum!(methods_general: A, B, C, D, E, F);
-}
-
-/// # Field-specific methods.
-impl<A, B, C, D, E, F> Enum<A, B, C, D, E, F> {
-    impl_enum!(methods_field_access: A, B, C, D, E, F);
-    impl_enum!(methods_map: A, B, C, D, E, F);
-}
-
-/// helper for implementing methods for Enum
-///
-/// Args:
-/// - $T: the uppercase generic identifier
+/// Defines [`Enum`] and implements all the methods.
+//
+// Supports >= 4 variants.
 macro_rules! impl_enum {
+    ($A:ident:$_a:literal, $B:ident:$_b:literal, $C:ident:$_c:literal,
+     $($T:ident:$nth:literal),+) => {
+        impl_enum!(define_enum: $A, $B, $C, $($T:$nth),+);
+        impl_enum!(impl_default: $A, $B, $C, $($T),+);
+        impl_enum!(methods_general: $A, $B, $C, $($T),+);
+        impl_enum!(methods_individual: $A, $B, $C, $($T),+);
+    };
+    (define_enum: $A:ident, $B:ident, $C:ident, $($rest:ident:$nth:literal),*) => { $crate::paste! {
+        /// A generic, parameterized *enum* for expressing structured alternatives.
+        ///
+        /// Variants are expected to be **contiguous**, meaning `()` (unit types)
+        /// should only appear at the **end**.
+        ///
+        /// The **first variant** (`A`) is considered the default,
+        /// implementing [`Default`] when `A: Default`.
+        #[non_exhaustive]
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+        pub enum Enum<$A, $B, $C=(), $($rest = ()),*> {
+            #[doc = "The 1st variant (default)."] $A($A),
+            #[doc = "The 2nd variant."] $B($B),
+            #[doc = "The 3rd variant."] $C($C),
+            $( #[doc = "The " $nth "th variant."] $rest($rest), )*
+        }
+    }};
     (
-    // implements:
+    impl_default: $A:ident $(, $rest:ident)*) => {
+        impl<$A: Default, $($rest),*> Default for Enum<$A, $($rest),*> {
+            fn default() -> Self { Enum::$A($A::default()) }
+        }
+    };
+    (
+    // Implements:
     // - len
     // - is_empty
     // - validate
@@ -67,30 +50,42 @@ macro_rules! impl_enum {
     // - variants_count
     // - eq_variant
     methods_general: $($T:ident),+) => {
-        /// Returns the number of active (non-`()` type) variants.
-        // WAIT: [const_type_id](https://github.com/rust-lang/rust/issues/77125)
-        // WAIT: [variant_count](https://github.com/rust-lang/rust/issues/73662)
-        pub fn len() -> usize {
-            let mut count = 0;
-            $( if <$T>::type_id() != <()>::type_id() { count += 1} )+
-            count
-        }
-        /// Returns `true` if all the variants are of the unit type.
-        pub fn is_empty() -> bool { Self::len() == 0 }
+        /// General methods.
+        impl< $($T: 'static),+ > Enum<$($T),+> {
+            /// Returns the number of active (non-`()` type) variants.
+            // WAIT: [const_type_id](https://github.com/rust-lang/rust/issues/77125)
+            // WAIT: [variant_count](https://github.com/rust-lang/rust/issues/73662)
+            pub fn len() -> usize {
+                let mut count = 0;
+                $( if <$T>::type_id() != <()>::type_id() { count += 1} )+
+                count
+            }
+            /// Returns `true` if all the variants are of the unit type.
+            pub fn is_empty() -> bool { Self::len() == 0 }
 
-        /// Returns the first non-unit variant name, if any.
-        // WAIT: [const_type_id](https://github.com/rust-lang/rust/issues/77125)
-        pub fn first_non_unit() -> Option<&'static str> {
-            $( if <$T>::type_id() != <()>::type_id() { return Some(stringify!($T)); } )+
-            None
-        }
+            /// Returns the first non-unit variant name, if any.
+            // WAIT: [const_type_id](https://github.com/rust-lang/rust/issues/77125)
+            pub fn first_non_unit() -> Option<&'static str> {
+                $( if <$T>::type_id() != <()>::type_id() { return Some(stringify!($T)); } )+
+                None
+            }
 
-        /// Validates that `()` variants only appear at the end.
-        #[allow(unused_assignments, reason = "wont be read in all cases")]
-        pub fn validate() -> bool {
-            let mut u = false; // unit variant found
-            $( if <$T>::type_id() == <()>::type_id() { u = true; } else if u { return false; } )+
-            true
+            /// Validates that `()` variants only appear at the end.
+            #[allow(unused_assignments, reason = "wont be read in all cases")]
+            pub fn validate() -> bool {
+                let mut u = false; // unit variant found
+                $( iif![<$T>::type_id() == <()>::type_id(); u = true; iif![u; return false]]; )+
+                true
+            }
+        }
+    };
+    (
+    // Implements methods acting over individual fields.
+    methods_individual: $($T:ident),+) => {
+        /// # Field-specific methods.
+        impl<$($T),+> Enum<$($T),+> {
+            impl_enum!(methods_field_access: $($T),+);
+            impl_enum!(methods_map: $($T),+);
         }
     };
     (
@@ -203,5 +198,17 @@ mod tests {
         let b: Enum<i32, f64> = Enum::B(3.14);
         assert_eq![Enum::B(3.14), b.map_a(|v| v * 2)];
         assert_eq![Enum::B(6.28), b.map_b(|v| v * 2.0)];
+    }
+    #[test]
+    fn field_access() {
+        let mut u = Unums::C(32);
+        assert_eq![u.is_c(), true];
+        assert_eq![u.into_c(), Some(32)];
+        assert_eq![u.as_ref_c(), Some(&32)];
+        assert_eq![u.as_mut_c(), Some(&mut 32)];
+        assert_eq![u.is_a(), false];
+        assert_eq![u.into_a(), None];
+        assert_eq![u.as_ref_a(), None];
+        assert_eq![u.as_mut_a(), None];
     }
 }
