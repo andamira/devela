939e407f8ffa77c89eca2b082db53e7dcdf48486
joseLuís <joseluis@andamira.net>
2025-04-27 13:00:22 +0200

    add `PREMUL` const-generic to `Rgba` type
    - add additional aliases.

--- src/media/color/rgb/definitions.rs
+++ src/media/color/rgb/definitions.rs
@@ -17,7 +17,7 @@ pub struct Rgb<T, const LINEAR: bool = false> {
 #[repr(C)]
 #[must_use]
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Rgba<T, const LINEAR: bool = false> {
+pub struct Rgba<T, const LINEAR: bool = false, const PREMUL: bool = false> {
     /// Color channels in order: [red, green, blue, alpha].
     ///
     /// - **Red/Green/Blue**: Gamma-encoded luminosity (0..=255).
@@ -31,11 +31,15 @@ pub struct Rgba<T, const LINEAR: bool = false> {
 pub type Rgb8 = Rgb<u8>;
 /// RGB+A color with 8-bit integer components (sRGB gamma space, straight alpha).
 pub type Rgba8 = Rgba<u8>;
+/// RGB+A color with 8-bit integer components (sRGB gamma space, premultiplied alpha).
+pub type RgbaPre8 = Rgba<u8, false, true>;
 
 /// RGB color with 16-bit integer components (sRGB gamma space).
 pub type Rgb16 = Rgb<u16>;
 /// RGB+A color with 16-bit integer components (sRGB gamma space, straight alpha).
 pub type Rgba16 = Rgba<u16>;
+/// RGB+A color with 16-bit integer components (sRGB gamma space, premultiplied alpha).
+pub type RgbaPre16 = Rgba<u16, false, true>;
 
 #[cfg(feature = "_float_f32")]
 crate::items! {
@@ -43,11 +47,15 @@ crate::items! {
     pub type RgbF32 = Rgb<f32>;
     /// RGB+A color with 32-bit float components (sRGB gamma space, straight alpha).
     pub type RgbaF32 = Rgba<f32>;
+    /// RGB+A color with 32-bit float components (sRGB gamma space, premultiplied alpha).
+    pub type RgbaPreF32 = Rgba<f32, false, true>;
 
     /// RGB color with 32-bit float components (linear space).
     pub type RgbLinF32 = Rgb<f32, true>;
     /// RGB+A color with 32-bit float components (linear space, straight alpha).
     pub type RgbaLinF32 = Rgba<f32, true>;
+    /// RGB+A color with 32-bit float components (linear space, premultiplied alpha).
+    pub type RgbaLinPreF32 = Rgba<f32, true, true>;
 }
 #[cfg(feature = "_float_f64")]
 crate::items! {
@@ -55,9 +63,13 @@ crate::items! {
     pub type RgbF64 = Rgb<f64>;
     /// RGB+A color with 64-bit float components (sRGB gamma space, straight alpha).
     pub type RgbaF64 = Rgba<f64>;
+    /// RGB+A color with 64-bit float components (sRGB gamma space, premultiplied alpha).
+    pub type RgbaPreF64 = Rgba<f64, false, true>;
 
     /// RGB color with 64-bit float components (linear space).
     pub type RgbLinF64 = Rgb<f64, true>;
     /// RGB+A color with 64-bit float components (linear space, straight alpha).
     pub type RgbaLinF64 = Rgba<f64, true>;
+    /// RGB+A color with 64-bit float components (linear space, premultiplied alpha).
+    pub type RgbaLinPreF64 = Rgba<f64, true, true>;
 }
