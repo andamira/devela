01b52b2086dd92e9d32d91611d7a9b11026c349e
joseLuís <joseluis@andamira.net>
2025-11-20 19:21:10 +0100

    update `KeyPad`
    - add variant: `Comma`.

--- src/ui/event/key/alpha_pad.rs
+++ src/ui/event/key/alpha_pad.rs
@@ -22,7 +22,7 @@ pub enum KeyAlpha {
 pub enum KeyPad {
     Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
     Multiply, Add, Subtract, Divide, Decimal,
-    Enter, Equal,
+    Enter, Equal, Comma,
 }
 
 /* impls */
@@ -116,6 +116,7 @@ impl KeyPad {
     /// Attempts to construct a `KeyPad` from a JavaScript `KeyboardEvent` physical [code].
     ///
     /// [code]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
+    // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_code_values
     pub const fn from_js_code(code: &str) -> Option<Self> {
         use KeyPad as K;
         match code.as_bytes() {
@@ -126,12 +127,14 @@ impl KeyPad {
             b"NumpadAdd" => Some(K::Add), b"NumpadSubtract" => Some(K::Subtract),
             b"NumpadDivide" => Some(K::Divide), b"NumpadDecimal" => Some(K::Decimal),
             b"NumpadEnter" => Some(K::Enter), b"NumpadEqual" => Some(K::Equal),
+            b"NumpadComma" => Some(K::Comma),
             _ => None,
         }
     }
     /// Returns a JavaScript `KeyboardEvent` physical [code].
     ///
     /// [code]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
+    // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_code_values
     pub const fn to_js_code(self) -> &'static str {
         use KeyPad as K;
         match self {
@@ -141,12 +144,14 @@ impl KeyPad {
             K::Num9 => "Numpad9", K::Multiply => "NumpadMultiply", K::Add => "NumpadAdd",
             K::Subtract => "NumpadSubtract", K::Divide => "NumpadDivide",
             K::Decimal => "NumpadDecimal", K::Enter => "NumpadEnter", K::Equal => "NumpadEqual",
+            K::Comma => "NumpadComma",
         }
     }
 
     /// Attempts to construct a `KeyPad` from a JavaScript `KeyboardEvent` semantic [key].
     ///
     /// [key]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+    // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
     pub const fn from_js_key(key: &str) -> Option<Self> {
         use KeyPad as K;
         match key.as_bytes() {
@@ -155,20 +160,21 @@ impl KeyPad {
             b"6" => Some(K::Num6), b"7" => Some(K::Num7), b"8" => Some(K::Num8),
             b"9" => Some(K::Num9), b"*" => Some(K::Multiply), b"+" => Some(K::Add),
             b"-" => Some(K::Subtract), b"/" => Some(K::Divide), b"." => Some(K::Decimal),
-            b"Enter" => Some(K::Enter), b"=" => Some(K::Equal),
+            b"Enter" => Some(K::Enter), b"=" => Some(K::Equal), b"," => Some(K::Comma),
             _ => None,
         }
     }
     /// Returns a JavaScript `KeyboardEvent` semantic [key].
     ///
     /// [key]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+    // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
     pub const fn to_js_key(self) -> &'static str {
         use KeyPad as K;
         match self {
             K::Num0 => "0", K::Num1 => "1", K::Num2 => "2", K::Num3 => "3", K::Num4 => "4",
             K::Num5 => "5", K::Num6 => "6", K::Num7 => "7", K::Num8 => "8", K::Num9 => "9",
             K::Multiply => "*", K::Add => "+", K::Subtract => "-", K::Divide => "/",
-            K::Decimal => ".", K::Enter => "Enter", K::Equal => "=",
+            K::Decimal => ".", K::Enter => "Enter", K::Equal => "=", K::Comma => ",",
         }
     }
 }
